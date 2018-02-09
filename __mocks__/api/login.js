1349b1ccd56bd098a90ff09b7606c54d0a4a51f3
Federico Locci <f.locci@entando.com>
2018-02-09 14:48:10 +0100

    add other test in for login

--- __mocks__/api/login.js
+++ __mocks__/api/login.js
@@ -1,2 +1,13 @@
+
+import { BODY_OK, BODY_ERROR } from 'test/mocks/login';
+
 // eslint-disable-next-line
-export const login = () => new Promise(resolve => resolve());
+export const login = (username, password) => {
+  return new Promise((resolve, reject) => {
+    if (username && password) {
+      resolve(BODY_OK);
+    } else {
+      reject(BODY_ERROR);
+    }
+  });
+};
--- src/api/login.js
+++ src/api/login.js
@@ -1,9 +1,13 @@
 
-import { BODY_OK } from 'test/mocks/login';
+import { BODY_OK, BODY_ERROR } from 'test/mocks/login';
 
 // eslint-disable-next-line
 export const login = (username, password) => {
-  return new Promise((resolve) => {
-    resolve(BODY_OK);
+  return new Promise((resolve, reject) => {
+    if (username && password) {
+      resolve(BODY_OK);
+    } else {
+      reject(BODY_ERROR);
+    }
   });
 };
