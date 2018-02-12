7770f2bc1870719803ec1666be7510d576beeb5b
Federico Locci <f.locci@entando.com>
2018-02-12 12:07:25 +0100

    remove comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import App from 'ui/app/components/App';
 import registerServiceWorker from 'registerServiceWorker';
 
 // state manager (Redux)
