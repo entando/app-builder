ac90fe385e78ebee95ac6da6c6f90c3e43bcf0d8
Nicola Puddu <n.puddu@entando.com>
2018-03-06 15:09:53 +0100

    removed the custom components for the kebab button and replaced with the one from patternfly.

--- src/ui/fragments/list/FragmentListMenuActions.js
+++ src/ui/fragments/list/FragmentListMenuActions.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Dropdown, MenuItem } from 'patternfly-react';
+import { DropdownKebab, MenuItem } from 'patternfly-react';
 import { LinkMenuItem } from 'frontend-common-components';
 
-import PageTreeActionMenuButton from 'ui/pages/common/PageTreeActionMenuButton';
 import { ROUTE_FRAGMENT_EDIT } from 'app-init/router';
 
 class FragmentListMenuActions extends Component {
@@ -26,29 +25,26 @@ class FragmentListMenuActions extends Component {
       <FormattedMessage id="fragment.table.edit" values={{ code: this.props.code }} />
     );
     return (
-      <Dropdown id="FragmentList-dropown">
-        <PageTreeActionMenuButton bsRole="toggle" />
-        <Dropdown.Menu className="dropdown-menu-right">
-          <LinkMenuItem
-            id={`edit-${this.props.code}`}
-            route={ROUTE_FRAGMENT_EDIT}
-            params={{ fragmentCode: this.props.code }}
-            label={editLabel}
-            className="FragmentListMenuAction__menu-item-edit"
-          />
-          <MenuItem
-            className="FragmentListMenuAction__menu-item-details"
-          >
-            <FormattedMessage id="fragment.table.details" values={{ code: this.props.code }} />
-          </MenuItem>
-          <MenuItem
-            className="FragmentListMenuAction__menu-item-delete"
-            onClick={this.handleClick(onClickDelete)}
-          >
-            <FormattedMessage id="app.delete" />
-          </MenuItem>
-        </Dropdown.Menu>
-      </Dropdown>
+      <DropdownKebab pullRight id="asdf">
+        <LinkMenuItem
+          id={`edit-${this.props.code}`}
+          route={ROUTE_FRAGMENT_EDIT}
+          params={{ fragmentCode: this.props.code }}
+          label={editLabel}
+          className="FragmentListMenuAction__menu-item-edit"
+        />
+        <MenuItem
+          className="FragmentListMenuAction__menu-item-details"
+        >
+          <FormattedMessage id="fragment.table.details" values={{ code: this.props.code }} />
+        </MenuItem>
+        <MenuItem
+          className="FragmentListMenuAction__menu-item-delete"
+          onClick={this.handleClick(onClickDelete)}
+        >
+          <FormattedMessage id="app.delete" />
+        </MenuItem>
+      </DropdownKebab>
     );
   }
 }
