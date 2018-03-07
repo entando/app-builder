dc09c3c62c055d15afc387d9abedf509ab8dde56
DMinnai <d.minnai@entando.com>
2018-03-07 16:58:17 +0100

    added more tests

--- src/ui/data-models/list/DataModelListTable.js
+++ src/ui/data-models/list/DataModelListTable.js
@@ -6,21 +6,19 @@ import { FormattedMessage } from 'react-intl';
 const DataModelListTable = ({ children }) => (
 
   <div className="DataModelListTable">
-    <div className="WidgetListTable__tables">
-      <table className="table table-striped table-bordered">
-        <thead>
-          <tr>
-            <th><FormattedMessage id="app.name" /></th>
-            <th className="text-center"><FormattedMessage id="app.type" /></th>
-            <th className="text-center" width="10%"><FormattedMessage id="app.id" /></th>
-            <th className="text-center" width="10%"><FormattedMessage id="app.actions" /></th>
-          </tr>
-        </thead>
-        <tbody>
-          {children}
-        </tbody>
-      </table>
-    </div>
+    <table className="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th><FormattedMessage id="app.name" /></th>
+          <th className="text-center"><FormattedMessage id="app.type" /></th>
+          <th className="text-center" width="10%"><FormattedMessage id="app.id" /></th>
+          <th className="text-center" width="10%"><FormattedMessage id="app.actions" /></th>
+        </tr>
+      </thead>
+      <tbody>
+        {children}
+      </tbody>
+    </table>
   </div>
 );
 
