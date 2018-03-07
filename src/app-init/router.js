b4f914309dce2bb6430cb93991eb0f17603b1d2a
Federico Locci <f.locci@entando.com>
2018-03-07 10:05:39 +0100

    fix suggestions

--- src/app-init/router.js
+++ src/app-init/router.js
@@ -18,7 +18,7 @@ export const ROUTE_WIDGET_EDIT = 'widgetEdit';
 export const ROUTE_FRAGMENT_ADD = 'fragmentAdd';
 export const ROUTE_FRAGMENT_EDIT = 'fragmentEdit';
 export const ROUTE_FRAGMENT_DETAIL = 'fragmentDetail';
-export const ROUTE_DATA_ADD = 'dataAdd';
+export const ROUTE_DATA_MODEL_ADD = 'dataModelAdd';
 
 
 routerConfig(
@@ -41,7 +41,7 @@ routerConfig(
       { name: ROUTE_FRAGMENT_ADD, path: '/fragment/add' },
       { name: ROUTE_FRAGMENT_EDIT, path: '/fragment/edit/:fragmentCode' },
       { name: ROUTE_FRAGMENT_DETAIL, path: '/fragment/view/:fragmentCode' },
-      { name: ROUTE_DATA_ADD, path: '/datamodel/add' },
+      { name: ROUTE_DATA_MODEL_ADD, path: '/datamodel/add' },
     ],
     notFoundRoute: { name: 'notFound', path: '/route-not-found' },
   },
--- src/state/data-types/selector.js
+++ src/state/data-types/selector.js
@@ -1,6 +1,5 @@
 import { createSelector } from 'reselect';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getDataTypes = state => state.dataTypes;
 
 export const getListDataTypes = createSelector(
