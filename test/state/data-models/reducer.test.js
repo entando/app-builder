20f2bf69c360e0a1e3410e5f80079057934e5730
DMinnai <d.minnai@entando.com>
2018-03-09 17:31:11 +0100

    pr fix

--- test/state/data-models/reducer.test.js
+++ test/state/data-models/reducer.test.js
@@ -1,23 +1,9 @@
 import reducer from 'state/data-models/reducer';
-import { setDataModels } from 'state/data-models/actions';
-import { DATA_MODELS } from 'test/mocks/dataModels';
 
 describe('state/data-types/reducer', () => {
   const state = reducer();
 
   it('should return an object', () => {
     expect(typeof state).toBe('object');
-  });
-
-  describe('SET_DATA_MODELS', () => {
-    let newState;
-    beforeEach(() => {
-      newState =
-      reducer(state, setDataModels(DATA_MODELS.payload));
-    });
-
-    it('compares the dataModel payload', () => {
-      expect(newState.list).toEqual(DATA_MODELS.payload);
-    });
   });
 });
