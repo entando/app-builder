3b642aa3c42e8a6b9c3169048ab61cff32b548d4
Nicola Puddu <n.puddu@entando.com>
2018-03-15 18:24:02 +0100

    added the redirect to the app in case the user is not logged in.

--- test/ui/app/AppContainer.test.js
+++ test/ui/app/AppContainer.test.js
@@ -2,12 +2,18 @@ import 'test/enzyme-init';
 
 import { mapStateToProps } from 'ui/app/AppContainer';
 
+jest.unmock('frontend-common-components');
+
 const TEST_STATE = {
   router: { route: 'page' },
+  currentUser: { username: 'admin' },
 };
 
 describe('AppContainer', () => {
   it('maps route property with state.router.route', () => {
-    expect(mapStateToProps(TEST_STATE)).toEqual({ route: 'page' });
+    expect(mapStateToProps(TEST_STATE)).toEqual({
+      route: 'page',
+      username: 'admin',
+    });
   });
 });
