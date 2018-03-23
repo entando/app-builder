03009f3e607c14d3f4ba14cc1832ed2eeba54fbc
DMinnai <d.minnai@entando.com>
2018-03-23 11:36:01 +0100

    pr fix

--- src/api/roles.js
+++ src/api/roles.js
@@ -1,6 +1,6 @@
 import { ROLES } from 'test/mocks/roles';
 
-// eslint-disable-next-line
+// eslint-disable-next-line import/prefer-default-export
 export const getApiRoles = () => (
   new Promise((resolve) => {
     resolve(ROLES);
