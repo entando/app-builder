7078699a5a5954ba67c48ef81f2b11d811033813
DMinnai <d.minnai@entando.com>
2018-03-29 08:50:40 +0200

    textareainput added

---
+++ test/ui/common/form/RenderTextAreaInput.test.js
@@ -0,0 +1,26 @@
+import 'test/enzyme-init';
+import { shallow } from 'enzyme';
+import RenderTextAreaInput from 'ui/common/form/RenderTextAreaInput';
+
+const INPUT = { name: 'textAreaInput', value: 'sample text' };
+const LABEL = 'Some Text';
+const META = { touched: true, error: '' };
+const META_WITH_ERROR = { touched: true, error: 'Required' };
+
+describe('RenderTextInput', () => {
+  let textInput;
+
+  it('render component without crash', () => {
+    const element = RenderTextAreaInput({ input: INPUT, label: LABEL, meta: META });
+    textInput = shallow(element);
+    expect(textInput.exists()).toEqual(true);
+  });
+
+  it('render error message for textInput', () => {
+    const element = RenderTextAreaInput({ input: INPUT, label: LABEL, meta: META_WITH_ERROR });
+    textInput = shallow(element);
+    const textInputHelpBlock = textInput.find('.help-block').first();
+    expect(textInputHelpBlock.exists()).toEqual(true);
+    expect(textInputHelpBlock.text()).toEqual('Required');
+  });
+});
