9645e28ba6f58a916c430ca446c8a3c41154bcfa
Davide Minnai <d.minnai@entando.com>
2018-03-29 15:00:51 +0200

    Update RenderTextAreaInput.test.js

--- test/ui/common/form/RenderTextAreaInput.test.js
+++ test/ui/common/form/RenderTextAreaInput.test.js
@@ -13,14 +13,14 @@ describe('RenderTextInput', () => {
   it('render component without crash', () => {
     const element = RenderTextAreaInput({ input: INPUT, label: LABEL, meta: META });
     textInput = shallow(element);
-    expect(textInput.exists()).toEqual(true);
+    expect(textInput.exists()).toBe(true);
   });
 
   it('render error message for textInput', () => {
     const element = RenderTextAreaInput({ input: INPUT, label: LABEL, meta: META_WITH_ERROR });
     textInput = shallow(element);
     const textInputHelpBlock = textInput.find('.help-block').first();
-    expect(textInputHelpBlock.exists()).toEqual(true);
-    expect(textInputHelpBlock.text()).toEqual('Required');
+    expect(textInputHelpBlock.exists()).toBe(true);
+    expect(textInputHelpBlock.text()).toBe('Required');
   });
 });
