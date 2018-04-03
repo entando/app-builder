cac6002f7c3c8d3054da4213171bb31c26ee8c4b
Davide Minnai <d.minnai@entando.com>
2018-04-03 16:34:40 +0200

    users + user  mended + uneeded files deleted (#83)

--- src/api/users.js
+++ src/api/users.js
@@ -1,12 +1,12 @@
 import { USERS_OK, USER_PROFILE_MOCK } from 'test/mocks/users';
-import { makeMockRequest, METHODS } from 'api/apiManager';
+import { makeRequest, METHODS } from 'api/apiManager';
 
 const getGenericError = obj => (
   obj || obj === '' ? [] : [{ code: 1, message: 'object is invalid' }]
 );
 
 export const getUsers = (page = { page: 1, pageSize: 10 }, params = '') => (
-  makeMockRequest(
+  makeRequest(
     {
       uri: `/api/users${params}`,
       method: METHODS.GET,
@@ -24,11 +24,27 @@ const getErrors = username => (
   ]
 );
 
-export const getUserDetail = username => makeMockRequest({
+export const getUserDetail = username => makeRequest({
   uri: `/api/users/${username}`,
   method: METHODS.GET,
   mockResponse: USER_PROFILE_MOCK[username],
+  useAuthentication: true,
   errors: () => getErrors(username),
+});
+
+export const getUser = params => makeRequest({
+  uri: `api/users/${params}`,
+  method: METHODS.GET,
+  mockResponse: USERS_OK,
+  useAuthentication: true,
+});
+
+export const putUser = user => makeRequest({
+  uri: `/api/users/${user.username}`,
+  method: METHODS.PUT,
+  body: user,
+  mockResponse: [],
+  useAuthentication: true,
 });
 
 export default getUsers;
--- test/api/users.test.js
+++ test/api/users.test.js
@@ -1,8 +1,8 @@
 import 'test/enzyme-init';
-import { getUsers } from 'api/users';
-import { USERS_OK } from 'test/mocks/users';
+import { getUsers, getUser, putUser } from 'api/users';
+import { USERS_OK, ERROR } from 'test/mocks/users';
 
-import { makeMockRequest, METHODS } from 'api/apiManager';
+import { makeRequest, METHODS } from 'api/apiManager';
 
 const correctRequest = {
   uri: '/api/users',
@@ -12,11 +12,26 @@ const correctRequest = {
   errors: expect.any(Function),
 };
 jest.mock('api/apiManager', () => ({
-  makeMockRequest: jest.fn(() => new Promise(resolve => resolve({}))),
+  makeRequest: jest.fn(() => new Promise(resolve => resolve({}))),
   METHODS: { GET: 'GET', POST: 'POST', PUT: 'PUT' },
 }));
 
 jest.unmock('api/users');
+
+const USER = {
+  username: 'login',
+  registration: '2018-01-08 00:00:00',
+  lastLogin: '2018-01-08 00:00:00',
+  lastPasswordChange: '2018-01-08 00:00:00',
+  status: 'active',
+};
+
+const USERS_BAD = {
+  payload: [],
+};
+const USER_BAD = {
+  payload: [],
+};
 
 describe('api/users', () => {
   beforeEach(() => {
@@ -28,9 +43,15 @@ describe('api/users', () => {
       expect(getUsers()).toBeInstanceOf(Promise);
     });
 
+    it('et an error response with wrong users payload', () => {
+      getUsers(USERS_BAD).then(() => {}, (error) => {
+        expect(error).toEqual(ERROR);
+      });
+    });
+
     it('get user page 1 as first page', () => {
       getUsers({ page: 1, pageSize: 10 });
-      expect(makeMockRequest).toHaveBeenCalledWith(
+      expect(makeRequest).toHaveBeenCalledWith(
         correctRequest,
         {
           page: 1,
@@ -38,5 +59,41 @@ describe('api/users', () => {
         },
       );
     });
+  });
+});
+describe('api/users', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getUser', () => {
+    it('returns a promise', () => {
+      expect(getUser()).toBeInstanceOf(Promise);
+    });
+
+    it('get an error response with wrog user payload', () => {
+      getUser(USER_BAD).then(() => {}, (error) => {
+        expect(error).toEqual(ERROR);
+      });
+    });
+  });
+});
+describe('putUser', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  it('returns a promise', () => {
+    expect(putUser(USER)).toBeInstanceOf(Promise);
+  });
+
+  it('makes the correct request with user body', () => {
+    putUser(USER);
+    expect(makeRequest).toHaveBeenCalledWith(expect.objectContaining({
+      method: METHODS.PUT,
+      uri: `/api/users/${USER.username}`,
+      body: USER,
+      useAuthentication: true,
+
+    }));
   });
 });
