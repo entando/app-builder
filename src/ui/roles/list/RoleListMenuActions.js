43d287447a3ecb0b23291522424221fa488c7000
m.carta <marcello.carta@realt.it>
2018-04-05 16:15:26 +0200

    EN-1129 fix PR comments

--- src/ui/roles/list/RoleListMenuActions.js
+++ src/ui/roles/list/RoleListMenuActions.js
@@ -1,54 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { DropdownKebab, MenuItem } from 'patternfly-react';
-// import { LinkMenuItem } from 'frontend-common-components';
-// import { ROUTE_GROUP_EDIT, ROUTE_GROUP_DETAIL } from 'app-init/router';
+import { LinkMenuItem } from 'frontend-common-components';
+import { ROUTE_ROLE_EDIT, ROUTE_ROLE_DETAIL } from 'app-init/router';
 
-class RoleListMenuActions extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+const RoleListMenuActions = ({ onClickDelete, code }) => {
+  const editLabel = <FormattedMessage id="app.edit" />;
+  const editDetails = <FormattedMessage id="app.details" />;
 
-  handleClick(handler) {
-    return (ev) => {
-      ev.preventDefault();
-      if (handler) { handler(this.props); }
-    };
-  }
-
-  render() {
-    const { onClickDelete } = this.props;
-
-    // const editLabel = <FormattedMessage id="app.edit" />;
-    // const editDetails = <FormattedMessage id="app.details" />;
-    return (
-      <DropdownKebab pullRight id={`${this.props.code}-actions`}>
-        {/* <LinkMenuItem
-          id={`detail-${this.props.code}`}
-          route={ROUTE_GROUP_DETAIL}
-          params={{ groupname: this.props.code }}
-          label={editDetails}
-          className="RoleListMenuAction__menu-item-detail"
-        />
-        <LinkMenuItem
-          id={`edit-${this.props.code}`}
-          route={ROUTE_GROUP_EDIT}
-          params={{ groupCode: this.props.code }}
-          label={editLabel}
-          className="RoleListMenuAction__menu-item-edit"
-        /> */}
-        <MenuItem
-          className="RoleListMenuAction__menu-item-delete"
-          onClick={this.handleClick(onClickDelete)}
-        >
-          <FormattedMessage id="app.delete" />
-        </MenuItem>
-      </DropdownKebab>
-    );
-  }
-}
+  return (
+    <DropdownKebab pullRight id={`${code}-actions`}>
+      <LinkMenuItem
+        id={`detail-${code}`}
+        route={ROUTE_ROLE_DETAIL}
+        params={{ roleCode: code }}
+        label={editDetails}
+        className="RoleListMenuAction__menu-item-detail"
+      />
+      <LinkMenuItem
+        id={`edit-${code}`}
+        route={ROUTE_ROLE_EDIT}
+        params={{ roleCode: code }}
+        label={editLabel}
+        className="RoleListMenuAction__menu-item-edit"
+      />
+      <MenuItem
+        className="RoleListMenuAction__menu-item-delete"
+        onClick={onClickDelete}
+      >
+        <FormattedMessage id="app.delete" />
+      </MenuItem>
+    </DropdownKebab>
+  );
+};
 
 RoleListMenuActions.propTypes = {
   onClickDelete: PropTypes.func,
--- src/ui/roles/list/RoleListTableContainer.js
+++ src/ui/roles/list/RoleListTableContainer.js
@@ -6,9 +6,6 @@ import { getLoading } from 'state/loading/selectors';
 import { getCurrentPage, getTotalItems, getPageSize } from 'state/pagination/selectors';
 import RoleListTable from 'ui/roles/list/RoleListTable';
 import { toggleLoading } from 'state/loading/actions';
-// import { setVisibleModal, setInfo } from 'state/modal/actions';
-// import { MODAL_ID } from 'ui/roles/common/DeleteRoleModal';
-
 
 export const mapStateToProps = state => (
   {
@@ -25,10 +22,6 @@ export const mapDispatchToProps = dispatch => ({
     dispatch(toggleLoading('roles'));
     dispatch(fetchRoles(page));
   },
-  // onClickDelete: ({ code }) => {
-  //   dispatch(setVisibleModal(MODAL_ID));
-  //   dispatch(setInfo({ type: 'role', code }));
-  // },
 });
 
 const RoleListTableContainer = connect(mapStateToProps, mapDispatchToProps)(RoleListTable);
