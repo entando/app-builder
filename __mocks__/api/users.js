3af05adf157c7fab7f16d09e6d26fb7fd148a327
Federico Locci <federico.locci@gmail.com>
2018-04-23 22:41:21 +0200

    add save user (#127)
    * add save user
    
    * fix test
    
    * fix suggestions

---
+++ __mocks__/api/users.js
@@ -0,0 +1,7 @@
+import { mockApi } from 'test/testUtils';
+
+export const getUserDetail = jest.fn(mockApi({ payload: {} }));
+export const getUsers = jest.fn(mockApi({ payload: [] }));
+export const postUser = jest.fn(mockApi({ payload: {} }));
+export const putUser = jest.fn(mockApi({ payload: {} }));
+export const getUser = jest.fn(mockApi({ payload: {} }));
