73770fb91e9fca9632907851013e1097c8eec2b2
m.carta <marcello.carta@realt.it>
2018-05-10 12:45:32 +0200

    EN-1414 show tooltip on PageStatusIcon hover

--- src/ui/pages/common/PageStatusIcon.js
+++ src/ui/pages/common/PageStatusIcon.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 import { PAGE_STATUS_DRAFT, PAGE_STATUS_PUBLISHED, PAGE_STATUS_UNPUBLISHED } from 'state/pages/const';
 
-const PageStatusIcon = ({ status, differsFromPublished }) => {
+const PageStatusIcon = ({ status, differsFromPublished, title }) => {
   const classNameAr = ['fa fa-circle PageStatusIcon', `PageStatusIcon--${status}`];
   if (differsFromPublished) {
     classNameAr.push('PageStatusIcon--diff');
   }
-  return <i className={classNameAr.join(' ')} />;
+  return <i className={classNameAr.join(' ')} title={title} />;
 };
 
 PageStatusIcon.propTypes = {
@@ -17,10 +17,12 @@ PageStatusIcon.propTypes = {
     PAGE_STATUS_PUBLISHED,
     PAGE_STATUS_UNPUBLISHED]).isRequired,
   differsFromPublished: PropTypes.bool,
+  title: PropTypes.string,
 };
 
 PageStatusIcon.defaultProps = {
   differsFromPublished: false,
+  title: '',
 };
 
 export default PageStatusIcon;
