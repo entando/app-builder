d91abcf699112802c02b6e5f9c32811cf22b7731
Dminnai <d.minnai@entando.com>
2018-05-15 11:33:58 +0200

    layout list added

---
+++ src/ui/profile-types/common/ProfileTypeStatusIcon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const ProfileTypeStatusIcon = ({ status, title }) => (
+  <i className={`fa ProfileTypeStatusIcon ProfileTypeStatusIcon--${status}`} title={title} />
+);
+
+ProfileTypeStatusIcon.propTypes = {
+  status: PropTypes.oneOf(['ok', 'ko', 'wip']).isRequired,
+  title: PropTypes.string,
+};
+
+ProfileTypeStatusIcon.defaultProps = {
+  title: '',
+};
+
+export default ProfileTypeStatusIcon;
