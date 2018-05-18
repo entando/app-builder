35cbca8332ce25ff6ff1c31ab4637d0753b43177
Marcello Carta <marcello.carta@realt.it>
2018-05-18 14:27:10 +0200

    EN-1445 fix page status icon, fix status titles (#184)

--- src/ui/pages/common/PageStatusIcon.js
+++ src/ui/pages/common/PageStatusIcon.js
@@ -3,18 +3,9 @@ import PropTypes from 'prop-types';
 import { formattedText } from '@entando/utils';
 import { PAGE_STATUS_DRAFT, PAGE_STATUS_PUBLISHED, PAGE_STATUS_UNPUBLISHED } from 'state/pages/const';
 
-const PageStatusIcon = ({ status, differsFromPublished }) => {
-  let iconTitle = formattedText(`pages.status.${status}`, `${status}`, {});
+const PageStatusIcon = ({ status }) => {
+  const iconTitle = formattedText(`pages.status.${status}`, `${status}`, {});
   const classNameAr = ['fa fa-circle PageStatusIcon', `PageStatusIcon--${status}`];
-  if (differsFromPublished) {
-    classNameAr.push('PageStatusIcon--diff');
-    iconTitle = [
-      `${formattedText('pages.status.draft')}`,
-      `${decodeURI('\u2260')}`,
-      `${formattedText('pages.status.published')}`,
-    ].join(' ');
-  }
-
   return <i className={classNameAr.join(' ')} title={iconTitle} />;
 };
 
@@ -23,11 +14,6 @@ PageStatusIcon.propTypes = {
     PAGE_STATUS_DRAFT,
     PAGE_STATUS_PUBLISHED,
     PAGE_STATUS_UNPUBLISHED]).isRequired,
-  differsFromPublished: PropTypes.bool,
-};
-
-PageStatusIcon.defaultProps = {
-  differsFromPublished: false,
 };
 
 export default PageStatusIcon;
