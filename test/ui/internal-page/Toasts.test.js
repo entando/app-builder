f30b440499d39c79a48681775cab6ae4f3003722
Nicola Puddu <n.puddu@entando.com>
2018-05-22 10:11:51 +0200

    added toast component to the internal page

---
+++ test/ui/internal-page/Toasts.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'test/enzyme-init';
+import { shallow } from 'enzyme';
+import { ToastNotificationList } from 'patternfly-react';
+
+import Toasts from 'ui/internal-page/Toasts';
+
+const component = shallow(<Toasts onDismiss={() => {}} />);
+
+
+describe('Toasts', () => {
+  it('renders without crashing', () => {
+    expect(component.exists()).toBe(true);
+  });
+
+  it('verify it is a ToastNotificationList', () => {
+    expect(component.type()).toBe(ToastNotificationList);
+  });
+});
