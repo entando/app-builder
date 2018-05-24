6fc3345c370052888a712fe3965c5820e8d71d0a
Nicola Puddu <n.puddu@entando.com>
2018-05-24 09:37:06 +0200

    updated the container

--- src/ui/pages/common/PageSettingsFormContainer.js
+++ src/ui/pages/common/PageSettingsFormContainer.js
@@ -1,27 +1,24 @@
 import { connect } from 'react-redux';
 
-// import the Component to be connected
 import PageSettingsForm from 'ui/pages/common/PageSettingsForm';
-import { fetchPageSettings, fetchFreePages } from 'state/pages/actions';
+import { fetchPageSettings, fetchFreePages, sendPutPageSettings } from 'state/pages/actions';
 import { getFreePages } from 'state/pages/selectors';
 
 export const mapStateToProps = state => ({
   options: getFreePages(state),
 });
 
-// map the props
 export const mapDispatchToProps = dispatch => ({
   onWillMount: () => {
-    // get select options from API
     dispatch(fetchFreePages());
-    // get page settings from API
     dispatch(fetchPageSettings());
   },
-  onSubmit: () => {},
+  onSubmit: (pageSettings) => {
+    const data = {
+      params: Object.keys(pageSettings).map(key => ({ [key]: pageSettings[key] })),
+    };
+    dispatch(sendPutPageSettings(data));
+  },
 });
 
-// connect the component
-const PageSettingsFormContainer = connect(mapStateToProps, mapDispatchToProps)(PageSettingsForm);
-
-// export connected component (Container)
-export default PageSettingsFormContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(PageSettingsForm);
