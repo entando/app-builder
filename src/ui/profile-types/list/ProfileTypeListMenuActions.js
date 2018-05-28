d57da8ce02256ab2c6885d83f13dc1eed5ca48f4
Nicola Puddu <n.puddu@entando.com>
2018-05-28 15:30:11 +0200

    removed handleClick

--- src/ui/profile-types/list/ProfileTypeListMenuActions.js
+++ src/ui/profile-types/list/ProfileTypeListMenuActions.js
@@ -1,53 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { LinkMenuItem } from 'frontend-common-components';
 import { DropdownKebab, MenuItem } from 'patternfly-react';
 import { ROUTE_PROFILE_TYPE_EDIT } from 'app-init/router';
 
-class ProfileTypeListMenuActions extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(handler) {
-    return (ev) => {
-      ev.preventDefault();
-      if (handler) { handler(this.props.code); }
-    };
-  }
-
-  render() {
-    const { onClickDelete } = this.props;
-    const editLabel = (
-      <FormattedMessage id="app.edit" values={{ code: this.props.code }} />
-    );
-    return (
-      <DropdownKebab pullRight id={`${this.props.code}-actions`}>
-        <LinkMenuItem
-          id={`profileType-${this.props.code}`}
-          route={ROUTE_PROFILE_TYPE_EDIT}
-          params={{ profiletypeCode: this.props.code }}
-          label={editLabel}
-          className="ProfileTypeListMenuAction__menu-item-edit"
-        />
-        <MenuItem
-          className="ProfileTypeListMenuAction__menu-item-reload"
-          onClick={this.handleClick()}
-        >
-          <FormattedMessage id="app.reload" />
-        </MenuItem>
-        <MenuItem
-          className="ProfileTypeListMenuAction__menu-item-delete"
-          onClick={this.handleClick(onClickDelete)}
-        >
-          <FormattedMessage id="app.delete" />
-        </MenuItem>
-      </DropdownKebab>
-    );
-  }
-}
+const ProfileTypeListMenuActions = ({ onClickDelete, code }) => {
+  const editLabel = (
+    <FormattedMessage id="app.edit" values={{ code }} />
+  );
+  return (
+    <DropdownKebab pullRight id={`${code}-actions`}>
+      <LinkMenuItem
+        id={`profileType-${code}`}
+        route={ROUTE_PROFILE_TYPE_EDIT}
+        params={{ profiletypeCode: code }}
+        label={editLabel}
+        className="ProfileTypeListMenuAction__menu-item-edit"
+      />
+      <MenuItem
+        className="ProfileTypeListMenuAction__menu-item-reload"
+      >
+        <FormattedMessage id="app.reload" />
+      </MenuItem>
+      <MenuItem
+        className="ProfileTypeListMenuAction__menu-item-delete"
+        onClick={() => (onClickDelete(code))}
+      >
+        <FormattedMessage id="app.delete" />
+      </MenuItem>
+    </DropdownKebab>
+  );
+};
 
 ProfileTypeListMenuActions.propTypes = {
   onClickDelete: PropTypes.func,
