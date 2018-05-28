2030c57233090f8a103820e183dbe8dfac133444
Nicola Puddu <n.puddu@entando.com>
2018-05-28 14:18:04 +0200

    added modal and fixed status column

--- src/ui/profile-types/common/ProfileTypeStatusIcon.js
+++ src/ui/profile-types/common/ProfileTypeStatusIcon.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Icon } from 'patternfly-react';
 
-const ProfileTypeStatusIcon = ({ status, title }) => (
-  <i className={`fa ProfileTypeStatusIcon ProfileTypeStatusIcon--${status}`} title={title} />
-);
+const ProfileTypeStatusIcon = ({ status, title }) => {
+  let icon;
+  switch (status) {
+    case 1:
+      icon = 'spinner';
+      break;
+    case 2:
+      icon = 'exclamation';
+      break;
+    default:
+      icon = 'check';
+  }
+  return (
+    <Icon title={title} name={icon} type="fa" />
+  );
+};
 
 ProfileTypeStatusIcon.propTypes = {
-  status: PropTypes.oneOf(['ok', 'ko', 'wip']).isRequired,
+  status: PropTypes.oneOf(['0', '1', '2']).isRequired,
   title: PropTypes.string,
 };
 
