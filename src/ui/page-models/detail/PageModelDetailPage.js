b57d3e54bb12d2a69206eb461949c5f207685255
Federico Locci <federico.locci@gmail.com>
2018-06-01 10:22:40 +0200

    add component  DragDropContext to class and amend the test (#252)

--- src/ui/page-models/detail/PageModelDetailPage.js
+++ src/ui/page-models/detail/PageModelDetailPage.js
@@ -4,6 +4,8 @@ import { FormattedMessage } from 'react-intl';
 import { Grid, Row, Col, Breadcrumb, Button } from 'patternfly-react';
 import { Link } from '@entando/router';
 import { BreadcrumbItem } from 'frontend-common-components';
+import { DragDropContextProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 import InternalPage from 'ui/internal-page/InternalPage';
 import PageTitle from 'ui/internal-page/PageTitle';
@@ -12,58 +14,60 @@ import SelectedPageModelDetailTableContainer from 'ui/page-models/detail/Selecte
 import PageModelPageReferencesTableContainer from 'ui/page-models/detail/PageModelPageReferencesTableContainer';
 
 const PageModelDetailPage = ({ pageModelCode }) => (
-  <InternalPage className="PageModelDetailPage">
-    <Grid fluid>
-      <Row>
-        <Col xs={12}>
-          <Breadcrumb>
-            <BreadcrumbItem active>
-              <FormattedMessage id="menu.pageDesigner" />
-            </BreadcrumbItem>
-            <BreadcrumbItem route={ROUTE_PAGE_MODEL_LIST}>
-              <FormattedMessage id="menu.pageModels" />
-            </BreadcrumbItem>
-            <BreadcrumbItem active>
-              <FormattedMessage id="menu.pageModelDetails" />
-            </BreadcrumbItem>
-          </Breadcrumb>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12}>
-          <PageTitle titleId="menu.pageModelDetails" helpId="pageModels.help" />
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12}>
-          <SelectedPageModelDetailTableContainer />
-        </Col>
-      </Row>
-      <br />
-      <Row>
-        <Col xs={12}>
-          <Link route={ROUTE_PAGE_MODEL_EDIT} params={{ pageModelCode }}>
-            <Button
-              type="button"
-              className="pull-right PageModelDetailPage__edit-btn"
-              bsStyle="primary"
-            >
-              <FormattedMessage id="app.edit" />
-            </Button>
-          </Link>
-        </Col>
-      </Row>
-      <br />
-      <Row className="form-horizontal">
-        <label className="col-xs-2 control-label">
-          <FormattedMessage id="references.referencedPages" />
-        </label>
-        <Col xs={10}>
-          <PageModelPageReferencesTableContainer />
-        </Col>
-      </Row>
-    </Grid>
-  </InternalPage>
+  <DragDropContextProvider backend={HTML5Backend}>
+    <InternalPage className="PageModelDetailPage">
+      <Grid fluid>
+        <Row>
+          <Col xs={12}>
+            <Breadcrumb>
+              <BreadcrumbItem active>
+                <FormattedMessage id="menu.pageDesigner" />
+              </BreadcrumbItem>
+              <BreadcrumbItem route={ROUTE_PAGE_MODEL_LIST}>
+                <FormattedMessage id="menu.pageModels" />
+              </BreadcrumbItem>
+              <BreadcrumbItem active>
+                <FormattedMessage id="menu.pageModelDetails" />
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <PageTitle titleId="menu.pageModelDetails" helpId="pageModels.help" />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <SelectedPageModelDetailTableContainer />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col xs={12}>
+            <Link route={ROUTE_PAGE_MODEL_EDIT} params={{ pageModelCode }}>
+              <Button
+                type="button"
+                className="pull-right PageModelDetailPage__edit-btn"
+                bsStyle="primary"
+              >
+                <FormattedMessage id="app.edit" />
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+        <br />
+        <Row className="form-horizontal">
+          <label className="col-xs-2 control-label">
+            <FormattedMessage id="references.referencedPages" />
+          </label>
+          <Col xs={10}>
+            <PageModelPageReferencesTableContainer />
+          </Col>
+        </Row>
+      </Grid>
+    </InternalPage>
+  </DragDropContextProvider>
 );
 
 PageModelDetailPage.propTypes = {
