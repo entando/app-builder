fff975dd80413c043fc8e71d66076da25941c61f
Nicola Puddu <n.puddu@entando.com>
2018-06-07 11:40:46 +0200

    added new error labels for connection failure fixed actions in: activity-stream, categories, dashboard, data-models, data-types, login-form

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,6 +1,9 @@
 export default {
   locale: 'en',
   messages: {
+    'app.serverError': 'could not establish connection with {domain}',
+    'app.noJsonReturned': 'could not establish connection with {domain}',
+    'app.permissionDenied': 'your session expired',
     'app.welcome': 'Welcome to Entando, {name}!',
     'app.yes': 'Yes',
     'app.no': 'No',
