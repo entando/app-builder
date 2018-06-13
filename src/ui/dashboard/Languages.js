df70f92e0ddfba8ee83d10d7d8ff3c3dc926a361
Federico Locci <federico.locci@gmail.com>
2018-06-13 08:15:39 +0200

    create card Languages (#325)

---
+++ src/ui/dashboard/Languages.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  AggregateStatusCount,
+  Icon,
+  Button,
+} from 'patternfly-react';
+import { FormattedMessage } from 'react-intl';
+import { Clearfix } from 'react-bootstrap';
+
+import { Link } from '@entando/router';
+import { ROUTE_LABELS_AND_LANGUAGES } from 'app-init/router';
+
+class Languages extends Component {
+  componentWillMount() {
+    this.props.onWillMount();
+  }
+
+  render() {
+    return (
+      <Card accented>
+        <CardTitle>
+          <Icon size="lg" name="flag" />
+          <FormattedMessage id="dashboard.languages" />
+          <Button
+            bsStyle="primary"
+            className="pull-right"
+            componentClass={Link}
+            route={ROUTE_LABELS_AND_LANGUAGES}
+          >
+            <FormattedMessage id="app.details" />
+          </Button>
+          <Clearfix />
+        </CardTitle>
+        <CardBody>
+          <span className="separator" />
+          <AggregateStatusCount>
+            <b>{this.props.activeLanguages}</b> <FormattedMessage id="dashboard.activeLanguages" />
+          </AggregateStatusCount>
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+Languages.propTypes = {
+  onWillMount: PropTypes.func.isRequired,
+  activeLanguages: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]).isRequired,
+};
+
+export default Languages;
---
+++ test/ui/dashboard/Languages.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import 'test/enzyme-init';
+import { shallow } from 'enzyme';
+import Languages from 'ui/dashboard/Languages';
+import { Link } from '@entando/router';
+import { ROUTE_LABELS_AND_LANGUAGES } from 'app-init/router';
+
+const component = shallow(<Languages onWillMount={() => {}} activeLanguages={2} />);
+
+describe('Languages', () => {
+  it('renders without crashing', () => {
+    expect(component.exists()).toBe(true);
+  });
+
+  it('verify it is a Card', () => {
+    const element = component.find('Card');
+    expect(element).toHaveLength(1);
+  });
+
+  it('verify it contains a button', () => {
+    const element = component.find('Button');
+    expect(element).toHaveLength(1);
+    const props = element.props();
+    expect(props).toHaveProperty('route', ROUTE_LABELS_AND_LANGUAGES);
+    expect(props).toHaveProperty('componentClass', Link);
+  });
+});
