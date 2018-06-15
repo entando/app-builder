32825ee3b8c6a31f4c3e01eaba9ba077608b6c7d
Federico Locci <federico.locci@gmail.com>
2018-06-15 12:46:25 +0200

    create component and actions

--- src/state/widgets/types.js
+++ src/state/widgets/types.js
@@ -2,3 +2,4 @@ export const SET_WIDGET_LIST = 'widgets/set-widget-list';
 export const SET_SELECTED_WIDGET = 'widgets/set-selected-widget';
 export const REMOVE_WIDGET = 'widgets/remove-widget';
 export const SET_WIDGETS_TOTAL = 'widgets/set-widgets-total';
+export const SET_WIDGET_INFO = 'widgets/set-widget-info';
