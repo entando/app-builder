40312adc28e23081a841dd459749a03fe8525c3f
Federico Locci <federico.locci@gmail.com>
2018-06-19 12:37:22 +0200

    fixed issue for clone page (#359)

--- src/ui/pages/clone/PagesClonePage.js
+++ src/ui/pages/clone/PagesClonePage.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Grid, Row, Col, Breadcrumb } from 'patternfly-react';
 import { BreadcrumbItem } from 'frontend-common-components';
@@ -11,57 +10,41 @@ import CloneFormContainer from 'ui/pages/clone/CloneFormContainer';
 import ErrorsAlertContainer from 'ui/common/form/ErrorsAlertContainer';
 
 
-class PagesClonePage extends Component {
-  componentWillMount() {
-    if (this.props.onWillMount) this.props.onWillMount(this.props);
-  }
-
-  render() {
-    return (
-      <InternalPage className="PagesClonePage">
-        <Grid fluid>
-          <Row>
-            <Col xs={12}>
-              <Breadcrumb>
-                <BreadcrumbItem active>
-                  <FormattedMessage id="menu.pageDesigner" />
-                </BreadcrumbItem>
-                <BreadcrumbItem route={ROUTE_PAGE_TREE}>
-                  <FormattedMessage id="menu.pageTree" />
-                </BreadcrumbItem>
-                <BreadcrumbItem active>
-                  <FormattedMessage id="app.clone" />
-                </BreadcrumbItem>
-              </Breadcrumb>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <PageTitle titleId="app.clone" helpId="pageTreePage.help" />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <ErrorsAlertContainer />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <CloneFormContainer />
-            </Col>
-          </Row>
-        </Grid>
-      </InternalPage>
-    );
-  }
-}
-
-PagesClonePage.propTypes = {
-  onWillMount: PropTypes.func,
-};
-
-PagesClonePage.defaultProps = {
-  onWillMount: null,
-};
+const PagesClonePage = () => (
+  <InternalPage className="PagesClonePage">
+    <Grid fluid>
+      <Row>
+        <Col xs={12}>
+          <Breadcrumb>
+            <BreadcrumbItem active>
+              <FormattedMessage id="menu.pageDesigner" />
+            </BreadcrumbItem>
+            <BreadcrumbItem route={ROUTE_PAGE_TREE}>
+              <FormattedMessage id="menu.pageTree" />
+            </BreadcrumbItem>
+            <BreadcrumbItem active>
+              <FormattedMessage id="app.clone" />
+            </BreadcrumbItem>
+          </Breadcrumb>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <PageTitle titleId="app.clone" helpId="pageTreePage.help" />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <ErrorsAlertContainer />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <CloneFormContainer />
+        </Col>
+      </Row>
+    </Grid>
+  </InternalPage>
+);
 
 export default PagesClonePage;
