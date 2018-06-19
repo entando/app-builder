d93edafc51554a6595dbae246323010e43e0c330
Federico Locci <federico.locci@gmail.com>
2018-06-19 12:53:20 +0200

    [MASTER ]En 1359 widget details master (#355)
    * create component and actions
    
    * add test
    
    * fixed suggestions

--- src/state/widgets/selectors.js
+++ src/state/widgets/selectors.js
@@ -1,11 +1,12 @@
 import { createSelector } from 'reselect';
-import { get } from 'lodash';
+import { get, isEmpty, uniq } from 'lodash';
 
 export const getWidgets = state => state.widgets;
 export const getWidgetsTotal = state => state.widgets.total;
 export const getWidgetsIdList = state => state.widgets.list;
 export const getWidgetsMap = state => state.widgets.map;
 export const getSelectedWidget = state => state.widgets.selected;
+
 export const getSelectedWidgetDefaultUi = state => get(state.widgets.selected, 'guiFragments[0].defaultUi');
 
 export const getListWidget = createSelector(
@@ -24,3 +25,28 @@ export const getTypologyWidgetList = createSelector(getListWidget, widgetList =>
     }
     return acc;
   }, {}));
+
+export const getWidgetInfo = createSelector([getWidgets], (widget) => {
+  const { info } = widget;
+  if (!isEmpty(info)) {
+    const list = uniq(info.draftUtilizers.map(m => m.pageCode)
+      .concat(info.publishedUtilizers.map(m => m.pageCode)));
+    const data = list.reduce((acc, pageCode) => {
+      const draft = info.draftUtilizers.find(f => f.pageCode === pageCode);
+      const publish = info.publishedUtilizers.find(f => f.pageCode === pageCode);
+      acc[pageCode] = {
+        pageFullPath: (draft && draft.pageFullPath) || (publish && publish.pageFullPath),
+        draft: !isEmpty(draft),
+        frameDraft: draft && `${draft.frame} [${draft.frameIndex}]`,
+        publish: !isEmpty(publish),
+        framePublish: publish && `${publish.frame} [${publish.frameIndex}]`,
+      };
+      return acc;
+    }, {});
+    return {
+      code: info.code,
+      data,
+    };
+  }
+  return info;
+});
---
+++ src/ui/widgets/detail/DetailWidgetPage.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import InternalPage from 'ui/internal-page/InternalPage';
+import PageTitle from 'ui/internal-page/PageTitle';
+import { Grid, Row, Col, Breadcrumb } from 'patternfly-react';
+import BreadcrumbItem from 'ui/common/BreadcrumbItem';
+import ErrorsAlertContainer from 'ui/common/form/ErrorsAlertContainer';
+import { ROUTE_WIDGET_LIST } from 'app-init/router';
+import DetailWidgetElement from 'ui/widgets/detail/DetailWidgetElement';
+
+class DetailWidgetPage extends Component {
+  componentWillMount() {
+    this.props.onWillMount();
+  }
+  render() {
+    const { widgetInfo } = this.props;
+    return (
+      <InternalPage className="DetailWidgetPage">
+        <Grid fluid>
+          <Row>
+            <Col xs={12}>
+              <Breadcrumb>
+                <BreadcrumbItem>
+                  <FormattedMessage id="menu.uxPattern" />
+                </BreadcrumbItem>
+                <BreadcrumbItem route={ROUTE_WIDGET_LIST}>
+                  <FormattedMessage id="menu.uxPattern.widget" />
+                </BreadcrumbItem>
+                <BreadcrumbItem active>
+                  <FormattedMessage id="app.info" />
+                </BreadcrumbItem>
+              </Breadcrumb>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <PageTitle
+                titleId="widget.detail.title"
+                titleParam={{ widgetDescription: widgetInfo.code }}
+                helpId="widget.help"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <ErrorsAlertContainer />
+              <DetailWidgetElement widgetInfo={widgetInfo} />
+            </Col>
+          </Row>
+        </Grid>
+      </InternalPage>
+    );
+  }
+}
+
+DetailWidgetPage.propTypes = {
+  onWillMount: PropTypes.func.isRequired,
+  widgetInfo: PropTypes.shape({}).isRequired,
+};
+
+export default DetailWidgetPage;
