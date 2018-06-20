cd149573827fe95cf408ed0d8fd5d6542d5774ee
Federico Locci <federico.locci@gmail.com>
2018-06-20 10:13:58 +0200

    fix languages title and test

--- test/ui/widgets/edit/EditWidgetFormContainer.test.js
+++ test/ui/widgets/edit/EditWidgetFormContainer.test.js
@@ -3,6 +3,8 @@ import 'test/enzyme-init';
 import { mapStateToProps, mapDispatchToProps } from 'ui/widgets/edit/EditWidgetFormContainer';
 import { getGroupsList } from 'state/groups/selectors';
 import { getSelectedWidgetDefaultUi } from 'state/widgets/selectors';
+import { getActiveLanguages } from 'state/languages/selectors';
+import { LANGUAGES_LIST as LANGUAGES } from 'test/mocks/languages';
 
 const GROUP = {
   code: '1',
@@ -29,6 +31,12 @@ jest.mock('state/widgets/selectors', () => ({
 
 getSelectedWidgetDefaultUi.mockReturnValue('');
 
+jest.mock('state/languages/selectors', () => ({
+  getActiveLanguages: jest.fn(),
+}));
+
+getActiveLanguages.mockReturnValue(LANGUAGES);
+
 describe('EditWidgetFormContainer', () => {
   let props;
   describe('mapStateToProps', () => {
@@ -40,6 +48,7 @@ describe('EditWidgetFormContainer', () => {
       expect(props).toHaveProperty('mode', 'edit');
       expect(props).toHaveProperty('groups', [GROUP]);
       expect(props).toHaveProperty('defaultUIField');
+      expect(props).toHaveProperty('languages', LANGUAGES);
     });
   });
 
