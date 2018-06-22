6bf3eb35d37813993947270aa40b2e076d5ff437
Federico Locci <federico.locci@gmail.com>
2018-06-22 10:08:18 +0200

    add description in widget detail and amend tests (#377)

--- src/ui/widgets/detail/DetailWidgetPageContainer.js
+++ src/ui/widgets/detail/DetailWidgetPageContainer.js
@@ -2,13 +2,19 @@ import { connect } from 'react-redux';
 import { fetchWidgetInfo } from 'state/widgets/actions';
 import { getWidgetInfo } from 'state/widgets/selectors';
 import DetailWidgetPage from 'ui/widgets/detail/DetailWidgetPage';
+import { getDefaultLanguage } from 'state/languages/selectors';
+import { fetchLanguages } from 'state/languages/actions';
 
 export const mapStateToProps = state => ({
   widgetInfo: getWidgetInfo(state),
+  defaultLanguage: getDefaultLanguage(state),
 });
 
 export const mapDispatchToProps = dispatch => ({
-  onWillMount: () => dispatch(fetchWidgetInfo()),
+  onWillMount: () => {
+    dispatch(fetchWidgetInfo());
+    dispatch(fetchLanguages({ page: 1, pageSize: 0 }));
+  },
 });
 
 const DetailWidgetPageContainer =
