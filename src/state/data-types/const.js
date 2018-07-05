479a0c0c22198f73617409e8795afd105727d3d4
Dminnai <d.minnai@entando.com>
2018-07-05 15:38:53 +0200

    datatype corrected with tests

--- src/state/data-types/const.js
+++ src/state/data-types/const.js
@@ -3,13 +3,15 @@ export const TYPE_CHECKBOX = 'CheckBox';
 export const TYPE_DATE = 'Date';
 export const TYPE_ENUMERATOR = 'Enumerator';
 export const TYPE_ENUMERATOR_MAP = 'EnumeratorMap';
-export const TYPE_MONOLIST = 'Monolist';
 export const TYPE_COMPOSITE = 'Composite';
 export const TYPE_LIST = 'List';
+export const TYPE_MONOLIST = 'Monolist';
 export const TYPE_NUMBER = 'Number';
 export const TYPE_THREESTATE = 'ThreeState';
 export const TYPE_TIMESTAMP = 'Timestamp';
-
+export const TYPE_HYPERTEXT = 'Hypertext';
+export const TYPE_LONGTEXT = 'Longtext';
+export const TYPE_TEXT = 'Text';
 export const MODE_ADD = 'add';
 export const MODE_EDIT = 'edit';
 export const MODE_EDIT_COMPOSITE = 'editComposite';
