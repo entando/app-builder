c4e9b9c676a2cb999d17a81bd7cd8f678ba7d615
Federico Locci <federico.locci@gmail.com>
2018-07-16 12:03:41 +0200

    [5.0.1] - En 1694 Monolist Composite Attribute (#424)
    * start Monolist Composite attribute
    
    * amend actions and component
    
    * complete create new Add monolist composite anf fixing edit Monolist Composite
    
    * fixing test
    
    * fix test
    
    * add test
    
    * amended MonolistAttributeFormContainer and selector
    
    * amended actions fetchDataTypeAttribute
    
    * fixed suggestions

--- src/ui/common/form/MonolistAttributeForm.js
+++ src/ui/common/form/MonolistAttributeForm.js
@@ -12,210 +12,203 @@ import AttributeEnumSettings from 'ui/common/attributes/AttributeEnumSettings';
 import RenderTextInput from 'ui/common/form/RenderTextInput';
 import SwitchRenderer from 'ui/common/form/SwitchRenderer';
 import FormLabel from 'ui/common/form/FormLabel';
+import AttributeListTableComposite from 'ui/common/attributes/AttributeListTableComposite';
+import AttributeInfoComposite from 'ui/common/attributes/AttributeInfoComposite';
+
+import {
+  TYPE_BOOLEAN,
+  TYPE_CHECKBOX,
+  TYPE_THREESTATE,
+  TYPE_NUMBER,
+  TYPE_DATE,
+  TYPE_ENUMERATOR,
+  TYPE_ENUMERATOR_MAP,
+  TYPE_TEXT,
+  TYPE_COMPOSITE,
+  TYPE_MONOLIST,
+  MODE_ADD_MONOLIST_ATTRIBUTE_COMPOSITE,
+} from 'state/data-types/const';
 
 const NO_INFO_ATTRIBUTE = [
-  'Boolean',
-  'Checkbox',
-  'Threestate',
+  TYPE_BOOLEAN,
+  TYPE_CHECKBOX,
+  TYPE_THREESTATE,
 ];
 
 export class MonolistAttributeFormBody extends Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
   componentWillMount() {
     this.props.onWillMount(this.props);
   }
+  render() {
+    const {
+      attributeCode, selectedAttributeType, isIndexable, type, invalid,
+      submitting, onSubmit, mode, attributesList, onAddAttribute, onClickDelete, onMove,
+      compositeAttributes,
+    } = this.props;
+    const isMonoListComposite = mode === MODE_ADD_MONOLIST_ATTRIBUTE_COMPOSITE;
+    const attributeType = isMonoListComposite ? TYPE_COMPOSITE : type;
+    const renderIndexable = () => {
+      if (isIndexable) {
+        return (
+          <FormGroup>
+            <label htmlFor="indexable" className="col-xs-2 control-label">
+              <FormLabel labelId="app.indexable" />
+            </label>
+            <Col xs={4}>
+              <Field component={SwitchRenderer} name="indexable" />
+            </Col>
+          </FormGroup>
+        );
+      }
+      return '';
+    };
 
-   onSubmit = (ev) => {
-     ev.preventDefault();
-     this.props.handleSubmit();
-   };
+    const renderAttributeInfo = () => {
+      if (isMonoListComposite) {
+        return <AttributeInfoComposite />;
+      }
 
-   render() {
-     const {
-       attributeCode,
-       selectedAttributeType,
-       isIndexable,
-       type,
-       invalid,
-       submitting,
-       selectedAttribute,
-     } = this.props;
+      return (!NO_INFO_ATTRIBUTE.includes(type) ?
+        <fieldset className="no-padding">
+          <legend>
+            <FormattedMessage id="app.info" />
+            <div className="MonolistAttributeForm__required-fields text-right">
+               * <FormattedMessage id="app.fieldsRequired" />
+            </div>
+          </legend>
+          <Field
+            component={RenderTextInput}
+            name="type"
+            label={<FormLabel labelId="app.type" />}
+            disabled
+          />
+          {renderIndexable()}
+        </fieldset>
+        : null);
+    };
 
-     const renderNumberConf = () => {
-       if (type === 'Number') {
-         return (
-           <FormSection name="validationRules">
-             <AttributesNumber {...this.props} />
-           </FormSection>
-         );
-       }
-       return '';
-     };
+    const renderSelectedAttribute = () => {
+      switch (attributeType) {
+        case TYPE_TEXT: return (
+          <FormSection name="validationRules">
+            <AttributeHypeLongMonoTextSettings {...this.props} />
+          </FormSection>
+        );
+        case TYPE_NUMBER: return (
+          <FormSection name="validationRules">
+            <AttributesNumber {...this.props} />
+          </FormSection>
+        );
+        case TYPE_DATE: return (
+          <FormSection name="validationRules">
+            <AttributesDateSettings {...this.props} />
+          </FormSection>
+        );
+        case TYPE_ENUMERATOR: return (
+          <AttributeEnumSettings
+            enumeratorExtractorBeans={selectedAttributeType.enumeratorExtractorBeans}
+          />
+        );
+        case TYPE_ENUMERATOR_MAP: return (
+          <AttributeEnumMapSettings
+            enumeratorMapExtractorBeans={selectedAttributeType.enumeratorMapExtractorBeans}
+          />
+        );
+        case TYPE_COMPOSITE: {
+          return isMonoListComposite ?
+            <AttributeListTableComposite
+              {...this.props}
+              attributesList={attributesList}
+              compositeAttributes={compositeAttributes}
+              onAddAttribute={onAddAttribute}
+              onClickDelete={onClickDelete}
+              onMove={onMove}
+              invalid={invalid}
+              submitting={submitting}
+            /> : null;
+        }
+        default: return null;
+      }
+    };
 
-     const renderDateConf = () => {
-       if (type === 'Date') {
-         return (
-           <FormSection name="validationRules">
-             <AttributesDateSettings {...this.props} />
-           </FormSection>
-         );
-       }
-       return '';
-     };
+    const renderOgnlValidation = () => (
+      !isMonoListComposite ?
+        <FormSection name="validationRules">
+          <AttributeOgnlValidation />
+        </FormSection> : null
+    );
 
-     const renderTextConf = () => {
-       if (selectedAttributeType.textFilterSupported) {
-         return (
-           <FormSection name="validationRules">
-             <AttributeHypeLongMonoTextSettings {...this.props} />
-           </FormSection>
-         );
-       }
-       return '';
-     };
 
-     const renderEnumConf = () => {
-       if (type === 'Enumerator') {
-         return (
-           <AttributeEnumSettings {...this.props} />
-         );
-       }
-       return '';
-     };
-
-     const renderEnumMapConf = () => {
-       if (type === 'EnumeratorMap') {
-         return (
-           <AttributeEnumMapSettings {...this.props} />
-         );
-       }
-       return '';
-     };
-
-     const renderIndexable = () => {
-       if (isIndexable) {
-         return (
-           <FormGroup>
-             <label htmlFor="filterList" className="col-xs-2 control-label">
-               <FormLabel labelId="app.indexable" />
-             </label>
-             <Col xs={4}>
-               <Field component={SwitchRenderer} name="indexable" />
-             </Col>
-           </FormGroup>
-         );
-       }
-       return '';
-     };
-
-     const renderAttributeInfo = () => {
-       if (NO_INFO_ATTRIBUTE.includes(type)) {
-         return '';
-       }
-
-       return (
-         <fieldset className="no-padding">
-           <legend>
-             <FormattedMessage id="app.info" />
-             <div className="DataTypeForm__required-fields text-right">
-             * <FormattedMessage id="app.fieldsRequired" />
-             </div>
-           </legend>
-           <Field
-             component={RenderTextInput}
-             name="type"
-             label={<FormLabel labelId="app.type" />}
-             disabled
-           />
-           {renderIndexable()}
-         </fieldset>
-       );
-     };
-
-     return (
-       <div className="MonoListAttributeForm">
-         <Alert type="info">
-           <FormattedMessage id="app.working" />
-           &nbsp;{type},&nbsp;
-           <FormattedMessage id="app.element.of" />&nbsp;
-           {attributeCode}&nbsp;
-           ({selectedAttribute}).
-         </Alert>
-         <form onSubmit={this.onSubmit} className="form-horizontal">
-           <Row>
-             <Col xs={12}>
-               <fieldset className="no-padding">
-                 <FormSection name="nestedAttribute">
-                   {renderAttributeInfo()}
-                   {renderNumberConf()}
-                   {renderDateConf()}
-                   {renderTextConf()}
-                   {renderEnumConf()}
-                   {renderEnumMapConf()}
-                   <FormSection name="validationRules">
-                     <AttributeOgnlValidation />
-                   </FormSection>
-                 </FormSection>
-               </fieldset>
-             </Col>
-           </Row>
-           <br />
-           <Row>
-             <Col xs={12}>
-               <Button
-                 className="pull-right MonolistAttributeForm__continue--btn"
-                 type="submit"
-                 bsStyle="primary"
-                 disabled={invalid || submitting}
-               >
-                 <FormattedMessage id="app.continue" />
-               </Button>
-             </Col>
-           </Row>
-         </form>
-       </div>
-     );
-   }
+    return (
+      <div className="MonoListAttributeForm">
+        <Alert type="info">
+          <FormattedMessage id="app.working" />
+          {type},&nbsp;
+          <FormattedMessage id="app.element.of" />&nbsp;
+          {attributeCode}&nbsp;
+           ({isMonoListComposite ? TYPE_MONOLIST : selectedAttributeType}).
+        </Alert>
+        <form onSubmit={this.props.handleSubmit(onSubmit)} className="form-horizontal">
+          <Row>
+            <Col xs={12}>
+              <fieldset className="no-padding">
+                <FormSection name="nestedAttribute">
+                  {renderAttributeInfo()}
+                  {renderSelectedAttribute()}
+                  {renderOgnlValidation()}
+                </FormSection>
+              </fieldset>
+            </Col>
+          </Row>
+          <br />
+          <Row>
+            <Col xs={12}>
+              <Button
+                className="pull-right MonolistAttributeForm__continue--btn"
+                type="submit"
+                bsStyle="primary"
+                disabled={invalid || submitting}
+              >
+                <FormattedMessage id="app.continue" />
+              </Button>
+            </Col>
+          </Row>
+        </form>
+      </div>
+    );
+  }
 }
 
 MonolistAttributeFormBody.propTypes = {
   onWillMount: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onAddAttribute: PropTypes.func,
+  onClickDelete: PropTypes.func,
+  onMove: PropTypes.func,
   type: PropTypes.string,
   attributeCode: PropTypes.string,
-  selectedAttribute: PropTypes.string,
+  selectedAttributeType: PropTypes.string,
+  mode: PropTypes.string,
+  attributesList: PropTypes.arrayOf(PropTypes.string),
   isIndexable: PropTypes.bool,
   invalid: PropTypes.bool,
   submitting: PropTypes.bool,
-  selectedAttributeType: PropTypes.shape({
-    simple: PropTypes.bool,
-    searchableOptionSupported: PropTypes.bool,
-    indexableOptionSupported: PropTypes.bool,
-    textFilterSupported: PropTypes.bool,
-    dateFilterSupported: PropTypes.bool,
-    numberFilterSupported: PropTypes.bool,
-    enumeratorOptionsSupported: PropTypes.bool,
-    enumeratorMapOptionsSupported: PropTypes.bool,
-    listAttribute: PropTypes.bool,
-    enumeratorExtractorBeans: PropTypes.arrayOf(PropTypes.shape({})),
-    enumeratorMapExtractorBeans: PropTypes.arrayOf(PropTypes.shape({})),
-  }),
+  compositeAttributes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 MonolistAttributeFormBody.defaultProps = {
+  onAddAttribute: null,
+  onClickDelete: null,
+  onMove: null,
   invalid: false,
   submitting: false,
   isIndexable: false,
   type: '',
   attributeCode: '',
-  selectedAttribute: '',
-  selectedAttributeType: {
-    enumeratorExtractorBean: [],
-    enumeratorMapExtractorBeans: [],
-  },
+  selectedAttributeType: TYPE_MONOLIST,
+  mode: '',
+  attributesList: [],
 };
 
 const MonolistAttributeForm = reduxForm({
