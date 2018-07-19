a452728cbecaa2ff6abc8826ae85843851e91a7d
Federico Locci <federico.locci@gmail.com>
2018-07-19 11:26:39 +0200

    add behavior when on focus code input

--- src/ui/groups/add/AddFormContainer.js
+++ src/ui/groups/add/AddFormContainer.js
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux';
 import GroupForm from 'ui/groups/common/GroupForm';
-import { change } from 'redux-form';
+import { change, touch } from 'redux-form';
 import { sendPostGroup } from 'state/groups/actions';
 
 export const mapDispatchToProps = dispatch => ({
   // calls post api
   onSubmit: values => dispatch(sendPostGroup(values)),
-  onChangeName: name =>
-    dispatch(change('group', 'code', name.replace(/\W/g, '_').toLowerCase())),
+  onChangeName: (name) => {
+    dispatch(change('group', 'code', name.replace(/\W/g, '_').toLowerCase()));
+    dispatch(touch('group', 'code'));
+  },
+  onFocus: (name) => {
+    dispatch(touch('group', name));
+  },
 });
 
 const AddFormContainer = connect(null, mapDispatchToProps)(GroupForm);
