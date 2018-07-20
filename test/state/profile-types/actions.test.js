adf76b1a14b40419425f60a402b715eddd0238bd
Dminnai <d.minnai@entando.com>
2018-07-20 11:46:50 +0200

    profile type refactor on add

--- test/state/profile-types/actions.test.js
+++ test/state/profile-types/actions.test.js
@@ -2,7 +2,7 @@ import { isFSA } from 'flux-standard-action';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { gotoRoute, getParams } from '@entando/router';
-import { ADD_ERRORS } from '@entando/messages';
+import { ADD_ERRORS, ADD_TOAST } from '@entando/messages';
 
 import {
   ROUTE_PROFILE_TYPE_LIST,
@@ -162,8 +162,9 @@ describe('state/profile-types/actions ', () => {
         store.dispatch(sendPostProfileType(PROFILE_TYPES)).then(() => {
           expect(postProfileType).toHaveBeenCalled();
           const actions = store.getActions();
-          expect(actions).toHaveLength(1);
-          expect(actions[0]).toHaveProperty('type', ADD_ERRORS);
+          expect(actions).toHaveLength(3);
+          expect(actions[1]).toHaveProperty('type', ADD_TOAST);
+          expect(actions[1].payload).toHaveProperty('type', 'error');
           done();
         }).catch(done.fail);
       });
