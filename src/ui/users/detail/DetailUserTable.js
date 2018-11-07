eb77b0b543bd0fda6b5851b18c370f1dfdda7702
Federico Locci <federico.locci@gmail.com>
2018-11-07 14:38:42 +0100

    add information in user detail

--- src/ui/users/detail/DetailUserTable.js
+++ src/ui/users/detail/DetailUserTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isUndefined } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Button } from 'react-bootstrap';
 import { gotoRoute } from '@entando/router';
@@ -12,6 +13,13 @@ class DetailUserTable extends React.Component {
   }
   render() {
     const { user } = this.props;
+    if (isUndefined(user.profileAttributes)) {
+      user.profileAttributes = {
+        fullname: '',
+        email: '',
+      };
+    }
+    const { profileAttributes } = user;
     return (
       <div className="DetailUserTable">
         <table className="table table-striped table-bordered" >
@@ -29,7 +37,7 @@ class DetailUserTable extends React.Component {
                 <FormattedMessage id="user.table.fullName" />
               </th>
               <td>
-                {user.fullName || ''}
+                {profileAttributes.fullname}
               </td>
             </tr>
             <tr>
@@ -37,7 +45,7 @@ class DetailUserTable extends React.Component {
                 <FormattedMessage id="user.table.email" />
               </th>
               <td>
-                {user.email}
+                {profileAttributes.email}
               </td>
             </tr>
           </tbody>
@@ -54,14 +62,19 @@ class DetailUserTable extends React.Component {
   }
 }
 
+const profileAttributesType = {
+  fullname: PropTypes.string,
+  email: PropTypes.string,
+};
+
 DetailUserTable.propTypes = {
   onWillMount: PropTypes.func,
   user: PropTypes.shape({
     username: PropTypes.string,
-    fullName: PropTypes.string,
-    email: PropTypes.string,
+    profileAttributes: PropTypes.shape(profileAttributesType),
   }).isRequired,
 };
+
 
 DetailUserTable.defaultProps = {
   onWillMount: () => {},
