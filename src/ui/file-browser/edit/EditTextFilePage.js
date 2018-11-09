52c620b7ad00862d71f5bdf25024a15fc3be1a69
Federico Locci <federico.locci@gmail.com>
2018-11-09 17:03:49 +0100

    amend src/ui/file-browser/edit/EditTextFilePage.js

--- src/ui/file-browser/edit/EditTextFilePage.js
+++ src/ui/file-browser/edit/EditTextFilePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col, Breadcrumb } from 'patternfly-react';
 import { FormattedMessage } from 'react-intl';
-import { BreadcrumbItem } from 'frontend-common-components';
+import BreadcrumbItem from 'ui/common/BreadcrumbItem';
 
 import InternalPage from 'ui/internal-page/InternalPage';
 import PageTitle from 'ui/internal-page/PageTitle';
