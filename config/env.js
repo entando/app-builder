2916c346f8e97240a4f705eeb00d24d7de71170a
Federico Locci <federico.locci@gmail.com>
2018-11-19 11:07:02 +0100

    Fix env master (#494)
    * add information in user detail
    
    * package-lock.json
    
    * amend env.js

--- config/env.js
+++ config/env.js
@@ -9,13 +9,11 @@ delete require.cache[require.resolve('./paths')];
 
 const NODE_ENV = process.env.NODE_ENV;
 if (!NODE_ENV) {
-  throw new Error(
-    'The NODE_ENV environment variable is required but was not specified.'
-  );
+  throw new Error('The NODE_ENV environment variable is required but was not specified.');
 }
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-var dotenvFiles = [
+const dotenvFiles = [
   `${paths.dotenv}.${NODE_ENV}.local`,
   `${paths.dotenv}.${NODE_ENV}`,
   // Don't include `.env.local` for `test` environment
@@ -30,13 +28,11 @@ var dotenvFiles = [
 // that have already been set.  Variable expansion is supported in .env files.
 // https://github.com/motdotla/dotenv
 // https://github.com/motdotla/dotenv-expand
-dotenvFiles.forEach(dotenvFile => {
+dotenvFiles.forEach((dotenvFile) => {
   if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand')(
-      require('dotenv').config({
-        path: dotenvFile,
-      })
-    );
+    require('dotenv-expand')(require('dotenv').config({
+      path: dotenvFile,
+    }));
   }
 });
 
@@ -79,9 +75,9 @@ function getClientEnvironment(publicUrl) {
         PUBLIC_URL: publicUrl,
         USE_MOCKS: process.env.USE_MOCKS !== 'false',
         DOMAIN: process.env.DOMAIN || null,
-        CLIENT_ID: process.env.CLIENT_ID || 'client_id',
-        CLIENT_SECRET: process.env.CLIENT_SECRET || 'client_secret',
-      }
+        CLIENT_ID: process.env.CLIENT_ID || 'appbuilder',
+        CLIENT_SECRET: process.env.CLIENT_SECRET || 'appbuilder_secret',
+      },
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
