7224cb363a9b0b99ae683f205f5f7cb2c21fd548
otarza <otar.zakalashvili@gmail.com>
2018-11-22 17:55:44 +0400

    EN-2220 & EN-2253 added specific eslint disable statement. Removed some base64 string images from mock components.

--- src/api/digital-exchange/components.js
+++ src/api/digital-exchange/components.js
@@ -1,7 +1,7 @@
 import { LIST_DE_COMPONENTS_OK } from 'test/mocks/digital-exchange/components';
 import { makeRequest, METHODS } from '@entando/apimanager';
 
-// eslint-disable-next-line
+// eslint-disable-next-line import/prefer-default-export
 export const getDEComponents = (page = { page: 1, pageSize: 10 }, params = '') => (
   makeRequest(
     {
