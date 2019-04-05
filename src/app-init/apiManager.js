b0de5fde004e3abc8f3e767ace31ec499a754ab9
Jeff Go <44834266+jeffgo10@users.noreply.github.com>
2019-04-05 16:14:05 +0800

    EN-2411 enable multifilter OR (#537)
    * EN-2411 frontend-libraries/@entando-utils dependency update to enable multifilter ‘OR’ in DE
    
    * (EN-2411) - updated @entando/apimanager dependency version + update on config method

--- src/app-init/apiManager.js
+++ src/app-init/apiManager.js
@@ -3,10 +3,11 @@ import { config, setApi, useMocks } from '@entando/apimanager';
 import { addToast, TOAST_WARNING } from '@entando/messages';
 import { formattedText } from '@entando/utils';
 
+import { gotoRoute } from '@entando/router';
 import { ROUTE_HOME, ROUTE_DASHBOARD } from 'app-init/router';
 import pluginsArray from 'entando-plugins';
 
-config(store, ROUTE_HOME, ROUTE_DASHBOARD);
+config(store, () => gotoRoute(ROUTE_HOME), () => gotoRoute(ROUTE_DASHBOARD));
 store.dispatch(setApi({
   domain: process.env.DOMAIN,
   useMocks: process.env.USE_MOCKS,
