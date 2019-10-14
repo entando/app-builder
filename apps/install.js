9df930ceba0e7a3ce1e55f7f18d582dc800b06fa
Nicola Puddu <n.puddu@entando.com>
2019-10-14 15:00:13 +0200

    updated the creation script to allow for local packages

--- apps/install.js
+++ apps/install.js
@@ -1,20 +1,26 @@
+const program = require('commander');
 const Log = require('./Log');
-
-if (!process.argv[2]) {
-  Log.error('no app specified');
-  process.exit();
-}
-
-const appId = process.argv[2];
-const app = `@entando/${appId}`;
-Log.section(`installing ${app}`);
-
 const { execSync } = require('child_process');
-
-execSync(`npm install --no-save ${app}`, { stdio: [0, 1, 2] });
-
-Log.section(`adding ${app} to app-builder`);
-
 const createAppsImportFile = require('./createImportsFile');
 
-createAppsImportFile(app, appId);
+program.version('1.0.0')
+  .name('npm run app-install')
+  .arguments('<app>')
+  .option('-p, --packageName <packageName>', 'name of the package')
+  .option('-d, --dev <path>', 'install app in development')
+  .action((app, { dev, packageName }) => {
+    const appFullName = !dev ? `@entando/${app}` : app;
+    const appPackageName = packageName || appFullName;
+    Log.section(`installing ${appFullName}`);
+    if (!dev) {
+      execSync(`npm install --no-save ${appFullName}`, { stdio: [0, 1, 2] });
+    } else {
+      Log.info(`installing from ${dev}`).empty(1);
+      execSync(`npm link --only=production ${dev}`, { stdio: [0, 1, 2] });
+    }
+    Log.empty(1).success('installation complete');
+    Log.section(`adding ${appFullName} to app-builder`);
+    createAppsImportFile(appFullName, app, appPackageName);
+  });
+
+program.parse(process.argv);
