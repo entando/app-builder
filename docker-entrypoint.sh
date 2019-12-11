03200d53806018653a63cdda3af51b8dcee218f0
Irakli Chalagashvili <i.chalagashvili@entando.com>
2019-12-11 11:58:54 +0400

    ENG-6 Use home variable along with PUBLIC_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 set -eu
 echo INITIATING PROTOCOL
-BUILD_PATH=${REACT_APP_BUILD_PATH:-""}
-PUBLIC_URL=${REACT_APP_PUBLIC_URL:-""}
+
+PUBLIC_URL=${REACT_APP_PUBLIC_URL:-"/app-builder"}
 DOMAIN=${REACT_APP_DOMAIN:-""}
 
 ENV_JSON='{"REACT_APP_DOMAIN":"'"$DOMAIN"'","REACT_APP_PUBLIC_URL":"'"$PUBLIC_URL"'"}'
-
 ESCAPED_ENV_JSON=$(echo $ENV_JSON | sed 's/\"/\\\"/g' | sed 's/\//\\\//g' | tr -d '\n' | tr -d '[[:blank:]]')
-if [ "$BUILD_PATH" != "" ];
-then
-  echo INSIDE IF
-  sed -i 's/%REACT_APP_ENV%/'"$ESCAPED_ENV_JSON"'/g' $BUILD_PATH/index.html
-  echo FINISHED WRITE
-fi
+
+sed -i 's/%REACT_APP_ENV%/'"$ESCAPED_ENV_JSON"'/g' $HOME$PUBLIC_URL/index.html
+
 echo SCRIPT FINISHED
 exec "$@"
