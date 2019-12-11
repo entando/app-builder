4fde1fe68e082a25f3b3ac2ab3f5a515dff40969
Irakli Chalagashvili <i.chalagashvili@entando.com>
2019-12-11 14:19:19 +0400

    EN-6 fix annoying error message which appears during development

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ DOMAIN=${REACT_APP_DOMAIN:-""}
 ENV_JSON='{"REACT_APP_DOMAIN":"'"$DOMAIN"'"}'
 ESCAPED_ENV_JSON=$(echo $ENV_JSON | sed 's/\"/\\\"/g' | sed 's/\//\\\//g' | tr -d '\n' | tr -d '[[:blank:]]')
 
-sed -i 's/%REACT_APP_ENV%/'"$ESCAPED_ENV_JSON"'/g' $HOME/app-builder/index.html
+sed -i 's/"REACT_APP_ENV"/'"$ESCAPED_ENV_JSON"'/g' $HOME/app-builder/index.html
 
 exec "$@"
