309c0871a855112aee2a0bc0c30e551540e7b78f
Irakli Chalagashvili <i.chalagashvili@entando.com>
2019-12-20 19:01:36 +0400

    EN6-136 implement widget config forms directly from cms

--- src/ui/widgets/config/WidgetConfigForm.js
+++ src/ui/widgets/config/WidgetConfigForm.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { intlShape } from 'react-intl';
 import Form from 'react-jsonschema-form';
+import apps from 'entando-apps';
 
-const WidgetConfigForm = ({ onSubmit, widgetConfig }) =>
-  (widgetConfig && widgetConfig.schema ? (
+const widgetForms = apps.reduce((obj, app) => ({
+  ...obj,
+  ...app.id === 'cms' ? app.widgetForms : {},
+}), {});
+
+const CMS_WIDGETS_CONFIG_FORM_MAPPING = {
+  // single handpicked content
+  content_viewer: widgetForms.HandpickedContentsConfigFormContainer,
+  // list of contents defined by a query
+  content_viewer_list: widgetForms.ContentsQueryContainer,
+  // more than one handpicked content
+  row_content_viewer_list: widgetForms.HandpickedContentsConfigFormContainer,
+  search_result: null,
+};
+
+const CMS_WIDGETS_CONFIG_FORMS = Object.keys(CMS_WIDGETS_CONFIG_FORM_MAPPING);
+
+const WidgetConfigForm = ({
+  onSubmit, widgetCode, widgetConfig, pageCode, frameId, intl, history,
+}) => {
+  if (CMS_WIDGETS_CONFIG_FORMS.includes(widgetCode)) {
+    return React.createElement(
+      CMS_WIDGETS_CONFIG_FORM_MAPPING[widgetCode],
+      {
+        widgetConfig, widgetCode, pageCode, frameId, intl, history,
+      },
+      null,
+    );
+  }
+  return (widgetConfig && widgetConfig.schema ? (
     <Form
       schema={widgetConfig.schema}
       formData={widgetConfig.formData}
       onSubmit={onSubmit}
     />
   ) : '');
+};
 
 WidgetConfigForm.propTypes = {
   widgetConfig: PropTypes.shape({}),
+  widgetCode: PropTypes.string.isRequired,
+  pageCode: PropTypes.string.isRequired,
+  frameId: PropTypes.number.isRequired,
   onSubmit: PropTypes.func.isRequired,
+  intl: intlShape.isRequired,
+  history: PropTypes.shape({}).isRequired,
 };
 
 WidgetConfigForm.defaultProps = {
