29136f859d831f719819eb130dac6d27015b6568
Giuseppe de Santis <g.desantis@entando.com>
2020-01-15 12:06:53 +0100

    remove old ENABLE_DIGITAL_EXCHANGE_UI env var

--- kubernetes.sh
+++ kubernetes.sh
@@ -4,7 +4,6 @@ export PUBLIC_URL=/app-builder
 export DOMAIN=/entando-de-app
 export KEYCLOAK_JSON=/entando-de-app/keycloak.json
 export DIGITAL_EXCHANGE_UI_ENABLED=true
-export ENABLE_DIGITAL_EXCHANGE_UI=true
 export KEYCLOAK_ENABLED=true
 npm run build --production
 docker build -t entando/entando-app-builder-de:6.0.0-SNAPSHOT .
