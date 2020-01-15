9337abc73020066f7cb8e076c1bb739aa0eb83bf
Giuseppe de Santis <g.desantis@entando.com>
2020-01-15 14:44:58 +0100

    Delete kubernetes.sh

--- kubernetes.sh
+++
@@ -1,9 +0,0 @@
-export USE_MOCKS=false
-export PUBLIC_URL=/app-builder
-# we will need to change that to /
-export DOMAIN=/entando-de-app
-export KEYCLOAK_JSON=/entando-de-app/keycloak.json
-export DIGITAL_EXCHANGE_UI_ENABLED=true
-export KEYCLOAK_ENABLED=true
-npm run build --production
-docker build -t entando/entando-app-builder-de:6.0.0-SNAPSHOT .
