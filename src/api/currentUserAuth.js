a328ca988505bb21b81202cd9b6e7afb106974f4
Irakli Chalagashvili <i.chalagashvili@entando.com>
2020-05-18 19:36:31 +0400

    ENG-425 fetch user group permissions from api

--- src/api/currentUserAuth.js
+++ src/api/currentUserAuth.js
@@ -1,10 +1,10 @@
-import { makeMockRequest, METHODS } from '@entando/apimanager';
+import { makeRequest, METHODS } from '@entando/apimanager';
 import { USER_AUTH_PAYLOAD_OK } from 'test/mocks/currentUserAuth';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getCurrentUserAuth = () => (
-  makeMockRequest({
-    uri: '/api/myAuthorities',
+  makeRequest({
+    uri: '/api/users/userProfiles/myGroupPermissions',
     method: METHODS.GET,
     mockResponse: USER_AUTH_PAYLOAD_OK,
     useAuthentication: true,
