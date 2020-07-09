5ef322036acb77e22aa6926c3c0207706f274096
Jeff Go <j.go@entando.com>
2020-07-09 21:35:25 +0800

    ENG-767 App builder title’s version using package.json version

---
+++ src/helpers/versions.js
@@ -0,0 +1,4 @@
+const { APP_BUILDER_VERSION } = process.env;
+
+// eslint-disable-next-line import/prefer-default-export
+export const appBuilderVersion = APP_BUILDER_VERSION ? APP_BUILDER_VERSION.replace(/.([^.]+)$/, '') : '';
