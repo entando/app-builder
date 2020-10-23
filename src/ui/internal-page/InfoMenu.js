4c6bc962550d867bba43bdf4128dbc45ac35c96b
Irakli Chalagashvili <i.chalagashvili@entando.com>
2020-10-23 18:33:59 +0400

    ENG-1300 add new features

--- src/ui/internal-page/InfoMenu.js
+++ src/ui/internal-page/InfoMenu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { LinkMenuItem } from '@entando/menu';
 import { ROUTE_ABOUT, ROUTE_LICENSE } from 'app-init/router';
 import InfoDropdown from 'ui/internal-page/InfoDropdown';
 
-const InfoMenu = () => (
+const InfoMenu = ({ onStartTutorial }) => (
   <InfoDropdown key="infoDropdown">
     <LinkMenuItem
       id="info-menu-about"
@@ -16,7 +17,16 @@ const InfoMenu = () => (
       to={ROUTE_LICENSE}
       label={<FormattedMessage id="app.license" />}
     />
+    <LinkMenuItem
+      id="info-menu-start-tutorial"
+      onClick={onStartTutorial}
+      label={<FormattedMessage id="app.startTutorial" />}
+    />
   </InfoDropdown>
 );
+
+InfoMenu.propTypes = {
+  onStartTutorial: PropTypes.func.isRequired,
+};
 
 export default InfoMenu;
