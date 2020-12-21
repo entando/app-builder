c5ec096d068ab7f3973aa3978d4ec26eb0bdfcc4
Irakli Chalagashvili <i.chalagashvili@entando.com>
2020-12-21 20:00:48 +0400

    ENG-1146 let user choose new version if action failed

--- src/ui/component-repository/components/item/install-controls/FailedInstallState.js
+++ src/ui/component-repository/components/item/install-controls/FailedInstallState.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'patternfly-react';
+import { Button, MenuItem, SplitButton } from 'patternfly-react';
 import { FormattedMessage } from 'react-intl';
 
 import { componentType } from 'models/component-repository/components';
 
-const FailedInstallState = ({ component, onRetryAction }) => (
-  <div className="ComponentList__install-actions">
-    <span className="ComponentList__status ComponentList__status-danger">
-      <FormattedMessage
-        id={
-          `componentRepository.components.${component.installed ? 'failedUninstall' : 'failedInstall'}`
-        }
-      />
-    </span>
-    <Button
-      bsStyle="link"
-      className="ComponentList__uninstall"
-      onClick={onRetryAction}
-    >
-      <FormattedMessage id="componentRepository.components.retry" />
-    </Button>
-  </div>
-);
+const FailedInstallState = ({ component, onRetryAction }) => {
+  const showVersionDropdown = Array.isArray(component.versions) && component.versions.length > 1;
+  return (
+    <div className="ComponentList__install-actions ComponentList__failed-state">
+      <span className="ComponentList__status ComponentList__status-danger">
+        <FormattedMessage
+          id={
+            `componentRepository.components.${component.installed ? 'failedUninstall' : 'failedInstall'}`
+          }
+        />
+      </span>
+      <div className="ComponentListInstallButtons">
+        {
+          showVersionDropdown
+            ? (
+              <SplitButton
+                bsStyle="primary"
+                onSelect={version => onRetryAction(version)}
+                onClick={() => onRetryAction()}
+                id={component.code}
+                title={<FormattedMessage id="componentRepository.components.retry" />}
+              >
+                {component.versions.map(({ version }) => (
+                  <MenuItem key={version} eventKey={version}>{version}</MenuItem>
+                ))}
+              </SplitButton>
+            )
+            : (
+              <Button
+                bsStyle="primary"
+                onClick={() => onRetryAction()}
+              >
+                <FormattedMessage id="componentRepository.components.retry" />
+              </Button >
+            )
+          }
+      </div>
+    </div>
+  );
+};
 
 FailedInstallState.propTypes = {
   component: componentType.isRequired,
