8529d42d05e2d230dd6f42711d940196063bdbb0
Jeff Go <j.go@entando.com>
2021-02-02 20:12:12 +0800

    react table column resizable PoC

--- src/ui/page-templates/list/PageTemplateListTable.js
+++ src/ui/page-templates/list/PageTemplateListTable.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Col, Paginator, Spinner } from 'patternfly-react';
 import { FormattedMessage, intlShape, injectIntl } from 'react-intl';
+import ColumnResizer from 'react-column-resizer';
+import { useTable } from 'react-table';
 
 import PageTemplateListMenuActions from 'ui/page-templates/list/PageTemplateListMenuActions';
 import PageTemplateDeleteModalContainer from 'ui/page-templates/common/PageTemplateDeleteModalContainer';
 import paginatorMessages from 'ui/paginatorMessages';
+
+const ReactTableContext = React.createContext(null);
+
+const MakeTable = ({ tableInfo, children }) => {
+  const tableProps = useTable({
+    ...tableInfo,
+  });
+  return (
+    <ReactTableContext.Provider value={tableProps}>
+      <ReactTableContext.Consumer>
+        {children}
+      </ReactTableContext.Consumer>
+    </ReactTableContext.Provider>
+  );
+};
 
 class PageTemplateListTable extends Component {
   constructor(props) {
@@ -40,48 +57,90 @@ class PageTemplateListTable extends Component {
     ), {});
 
     return (
-      <Col xs={12}>
-        <table className="PageTemplateListTable__table table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th><FormattedMessage id="app.code" /></th>
-              <th><FormattedMessage id="app.name" /></th>
-              <th className="text-center" width="10%">
-                <FormattedMessage id="app.actions" />
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderRows()}
-          </tbody>
-        </table>
-        <Paginator
-          pagination={pagination}
-          viewType="table"
-          itemCount={this.props.totalItems}
-          onPageSet={this.changePage}
-          onPerPageSelect={this.changePageSize}
-          messages={messages}
-        />
-      </Col>
+      <MakeTable
+        tableInfo={{
+          // you can change the orders of column here,
+          columns: [
+            {
+              Header: intl.formatMessage({ id: 'app.code' }),
+              accessor: 'code',
+            },
+            {
+              Header: intl.formatMessage({ id: 'app.name' }),
+              accessor: 'descr',
+            },
+          ],
+          data: this.props.pageTemplates,
+        }}
+      >
+        {({
+          getTableProps,
+          getTableBodyProps,
+          headerGroups,
+          rows,
+          prepareRow,
+        }) => (
+          <Col xs={12}>
+            <table {...getTableProps()} className="PageTemplateListTable__table table table-striped table-bordered">
+              <thead>
+                <tr {...headerGroups[0].getHeaderGroupProps()}>
+                  {headerGroups[0].headers.map(column => ([
+                    <th
+                      {...column.getHeaderProps()}
+                    >
+                      {column.render('Header')}
+                    </th>,
+                    <ColumnResizer className="columnResizer" />,
+                  ]))}
+                  <th className="text-center" width="10%">
+                    <FormattedMessage id="app.actions" />
+                  </th>
+                </tr>
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {this.renderRows({ prepareRow, rows })}
+              </tbody>
+            </table>
+            <Paginator
+              pagination={pagination}
+              viewType="table"
+              itemCount={this.props.totalItems}
+              onPageSet={this.changePage}
+              onPerPageSelect={this.changePageSize}
+              messages={messages}
+            />
+          </Col>
+        )}
+      </MakeTable>
     );
   }
 
-  renderRows() {
+  renderRows({ prepareRow, rows }) {
     const { removePageTemplate } = this.props;
     return (
-      this.props.pageTemplates.map(pageTemplate => (
-        <tr key={pageTemplate.code}>
-          <td className="PageTemplateListTable__td">{pageTemplate.code}</td>
-          <td className="PageTemplateListTable__td">{pageTemplate.descr}</td>
-          <td className="PageTemplateListTable__td text-center">
-            <PageTemplateListMenuActions
-              code={pageTemplate.code}
-              onClickDelete={() => removePageTemplate(pageTemplate.code)}
-            />
-          </td>
-        </tr>
-      ))
+      rows.map((row) => {
+        prepareRow(row);
+        const { values } = row;
+        return (
+          <tr {...row.getRowProps()}>
+            {row.cells.map(pageTemplate => ([
+              <td
+                {...pageTemplate.getCellProps()}
+                className="PageTemplateListTable__td"
+              >
+                {pageTemplate.render('Cell')}
+              </td>,
+              <td className="colForResize" />,
+            ]))}
+            <td className="PageTemplateListTable__td text-center">
+              <PageTemplateListMenuActions
+                code={values.code}
+                onClickDelete={() => removePageTemplate(values.code)}
+              />
+            </td>
+          </tr>
+        );
+      })
     );
   }
 
