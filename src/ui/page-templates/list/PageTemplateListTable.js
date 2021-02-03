12c2820d4d25fe7369296b598634671570135492
Jeff Go <j.go@entando.com>
2021-02-03 15:40:51 +0800

    reorder columns via dragging poc

--- src/ui/page-templates/list/PageTemplateListTable.js
+++ src/ui/page-templates/list/PageTemplateListTable.js
@@ -11,6 +11,8 @@ import paginatorMessages from 'ui/paginatorMessages';
 
 const ReactTableContext = React.createContext(null);
 
+const perPageOptions = [5, 10, 15, 25, 50];
+
 const MakeTable = ({ tableInfo, children }) => {
   const tableProps = useTable({
     ...tableInfo,
@@ -28,12 +30,42 @@ class PageTemplateListTable extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      // you can change the orders of column here,
+      columns: [
+        {
+          Header: <FormattedMessage id="app.code" />,
+          accessor: 'code',
+        },
+        {
+          Header: <FormattedMessage id="app.name" />,
+          accessor: 'descr',
+        },
+        {
+          Header: 'Main Frame',
+          accessor: 'mainFrame',
+        },
+      ],
+      dragOver: '',
+    };
+
     this.changePage = this.changePage.bind(this);
     this.changePageSize = this.changePageSize.bind(this);
+    this.handleDragStart = this.handleDragStart.bind(this);
+    this.handleDragEnter = this.handleDragEnter.bind(this);
+    this.handleOnDrop = this.handleOnDrop.bind(this);
   }
 
   componentWillMount() {
     this.props.onWillMount();
+  }
+
+  setDragOver(dragOver) {
+    this.setState({ dragOver });
+  }
+
+  setColumns(columns) {
+    this.setState({ columns });
   }
 
   changePage(page) {
@@ -44,35 +76,51 @@ class PageTemplateListTable extends Component {
     this.props.onWillMount({ page: 1, pageSize });
   }
 
+  handleDragStart(ev) {
+    const { columns } = this.state;
+    const { id } = ev.target;
+    const idx = columns.findIndex(col => col.accessor === id);
+    ev.dataTransfer.setData('colIdx', idx);
+  }
+
+  handleDragOver = e => e.preventDefault();
+
+  handleDragEnter({ target }) {
+    const { id } = target;
+    this.setDragOver(id);
+  }
+
+  handleOnDrop(ev) {
+    const { id } = ev.target;
+    const { columns } = this.state;
+    const droppedColIdx = columns.findIndex(col => col.accessor === id);
+    const draggedColIdx = ev.dataTransfer.getData('colIdx');
+    const tempCols = [...columns];
+
+    tempCols[draggedColIdx] = columns[droppedColIdx];
+    tempCols[droppedColIdx] = columns[draggedColIdx];
+    this.setColumns(tempCols);
+    this.setDragOver('');
+  }
+
   renderTable() {
-    const { page, pageSize, intl } = this.props;
-    const pagination = {
+    const {
       page,
-      perPage: pageSize,
-      perPageOptions: [5, 10, 15, 25, 50],
-    };
+      pageSize,
+      intl,
+      pageTemplates,
+    } = this.props;
+
+    const { columns, dragOver } = this.state;
+
+    const pagination = { page, perPage: pageSize, perPageOptions };
 
     const messages = Object.keys(paginatorMessages).reduce((acc, curr) => (
       { ...acc, [curr]: intl.formatMessage(paginatorMessages[curr]) }
     ), {});
 
     return (
-      <MakeTable
-        tableInfo={{
-          // you can change the orders of column here,
-          columns: [
-            {
-              Header: intl.formatMessage({ id: 'app.code' }),
-              accessor: 'code',
-            },
-            {
-              Header: intl.formatMessage({ id: 'app.name' }),
-              accessor: 'descr',
-            },
-          ],
-          data: this.props.pageTemplates,
-        }}
-      >
+      <MakeTable tableInfo={{ columns, data: pageTemplates }}>
         {({
           getTableProps,
           getTableBodyProps,
@@ -87,6 +135,14 @@ class PageTemplateListTable extends Component {
                   {headerGroups[0].headers.map(column => ([
                     <th
                       {...column.getHeaderProps()}
+                      id={column.id}
+                      key={column.id}
+                      draggable
+                      onDragStart={this.handleDragStart}
+                      onDragOver={this.handleDragOver}
+                      onDrop={this.handleOnDrop}
+                      onDragEnter={this.handleDragEnter}
+                      dragOver={column.id === dragOver}
                     >
                       {column.render('Header')}
                     </th>,
