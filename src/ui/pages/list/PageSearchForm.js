8b940b98c8214f37e0df7f357965bf143262b335
Irakli Chalagashvili <chalagashvili5@gmail.com>
2021-02-12 20:41:01 +0400

    ENG-1771 implement pages search by name

--- src/ui/pages/list/PageSearchForm.js
+++ src/ui/pages/list/PageSearchForm.js
@@ -1,36 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { FormattedMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
-import { Row, Col, FormGroup, Button } from 'patternfly-react';
+import { Row, Col, FormGroup, Button, DropdownButton, MenuItem } from 'patternfly-react';
 
 const msgs = defineMessages({
   code: {
-    id: 'pageTree.searchForm.code',
-    defaultMessage: 'Code',
+    id: 'pageTree.searchForm.search.code',
+  },
+  name: {
+    id: 'pageTree.searchForm.search.name',
   },
 });
 
-export const PageSearchFormBody = ({ intl, handleSubmit }) => {
-  const onSubmit = (ev) => {
-    ev.preventDefault();
-    handleSubmit();
-  };
+const selectOptions = [
+  {
+    label: 'pageTree.searchForm.name',
+    value: 'name',
+  },
+  {
+    label: 'pageTree.searchForm.code',
+    value: 'code',
+  },
+];
+
+const selectedTypeSearchParamMap = {
+  code: 'pageCodeToken',
+  name: 'title',
+};
+
+export const PageSearchFormBody = ({ intl, handleSubmit, onSubmit }) => {
+  const [searchType, setSearchType] = useState('code');
   return (
-    <form onSubmit={onSubmit} className="PageSearchForm form-horizontal well">
-      <h3><FormattedMessage id="app.search" /></h3>
+    <form
+      onSubmit={handleSubmit(values =>
+        onSubmit({ ...values, searchType: selectedTypeSearchParamMap[searchType] }))}
+      className="PageSearchForm form-horizontal well"
+    >
+      <h3><FormattedMessage id="pageTree.searchForm.searchPageBy" /></h3>
       <FormGroup>
         <Row>
-          <label className="control-label col-xs-3" htmlFor="pagecode">
-            <FormattedMessage id="pageTree.searchForm.code" />
-          </label>
-          <Col xs={8}>
+          <Col xs={12} sm={3} className="PageSearchForm__filter-searchby">
+            <DropdownButton
+              title={intl.formatMessage({ id: `pageTree.searchForm.${searchType}` })}
+              id="attribute"
+              onSelect={e => setSearchType(e)}
+              className="PageSearchForm__filter-searchby-dropdown"
+            >
+              {selectOptions.map(option => (
+                <MenuItem key={option.value} eventKey={option.value}>
+                  {intl.formatMessage({ id: option.label })}
+                </MenuItem>
+                ))}
+            </DropdownButton>
+          </Col>
+          <Col xs={12} sm={8}>
             <Field
               id="pagecode"
               component="input"
               className="form-control"
               name="pageCodeToken"
-              placeholder={intl.formatMessage(msgs.code)}
+              placeholder={intl.formatMessage(msgs[searchType])}
             />
           </Col>
         </Row>
@@ -56,6 +86,7 @@ export const PageSearchFormBody = ({ intl, handleSubmit }) => {
 PageSearchFormBody.propTypes = {
   intl: intlShape.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 const PageSearchForm = reduxForm({
