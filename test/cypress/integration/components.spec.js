62d85a4a932b97c6150b2f2c527924ac71b4ac9d
Anthony Viard <anth.viard@gmail.com>
2021-04-02 18:21:18 +0200

    Add tests for components files upload

---
+++ test/cypress/integration/components.spec.js
@@ -0,0 +1,29 @@
+import {
+  TEST_ID_COMPONENT_LIST_BUTTON,
+  TEST_ID_ICON_UPLOADER_INPUT,
+} from '../../../src/ui/test-const/components-test-const';
+
+describe('Pages', () => {
+  beforeEach(() => {
+    cy.appBuilderLogin();
+  });
+
+  afterEach(() => {
+    cy.appBuilderLogout();
+  });
+
+  describe('Add a new widget with upload a file', () => {
+    it('Should create the widget and upload the icon', () => {
+      cy.openPageFromMenu(['Components', 'Micro Frontends & Widgets']);
+      cy.getByTestId(TEST_ID_COMPONENT_LIST_BUTTON).contains('Add').click();
+      cy.getByTestId(TEST_ID_ICON_UPLOADER_INPUT).attachFile('icon/pokeball.svg');
+    });
+
+    it('Should create the widget and drag and drop the icon', () => {
+      cy.openPageFromMenu(['Components', 'Micro Frontends & Widgets']);
+      cy.getByTestId(TEST_ID_COMPONENT_LIST_BUTTON).contains('Add').click();
+      cy.getByTestId(TEST_ID_ICON_UPLOADER_INPUT).attachFile('icon/pokeball.svg', { subjectType: 'drag-n-drop' });
+    });
+  });
+});
+
