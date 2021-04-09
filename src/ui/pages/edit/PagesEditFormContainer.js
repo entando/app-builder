f342ed53ee78213cbb40b42d5750ddf9f8453470
Giuseppe de Santis <gdesantis@gmail.com>
2021-04-09 18:34:51 +0200

    ENG-2222 filter groups for non-admins in page edit screen

--- src/ui/pages/edit/PagesEditFormContainer.js
+++ src/ui/pages/edit/PagesEditFormContainer.js
@@ -4,22 +4,26 @@ import { routeConverter } from '@entando/utils';
 
 import PageForm from 'ui/pages/common/PageForm';
 import { getActiveLanguages } from 'state/languages/selectors';
-import { getGroupsList } from 'state/groups/selectors';
+import { currentUserGroupsPermissionsFilter } from 'state/groups/selectors';
 import { getPageTemplatesList } from 'state/page-templates/selectors';
 import { getCharsets, getContentTypes, getPageTreePages } from 'state/pages/selectors';
 import { ACTION_SAVE, ACTION_SAVE_AND_CONFIGURE, SEO_ENABLED } from 'state/pages/const';
 import { sendPutPage, fetchPageForm } from 'state/pages/actions';
-import { fetchGroups } from 'state/groups/actions';
+import { fetchCurrentUserGroups } from 'state/groups/actions';
 import { fetchPageTemplates } from 'state/page-templates/actions';
 import { history, ROUTE_PAGE_TREE, ROUTE_PAGE_CONFIG } from 'app-init/router';
 import { fetchLanguages } from 'state/languages/actions';
 import { setVisibleModal } from 'state/modal/actions';
+import { MANAGE_PAGES_PERMISSION } from 'state/permissions/const';
 
 export const FORM_ID = 'pageEdit';
 
+const getCurrentUserGroupsWithManagePages =
+  currentUserGroupsPermissionsFilter([MANAGE_PAGES_PERMISSION]);
+
 export const mapStateToProps = (state, { match: { params } }) => ({
   languages: getActiveLanguages(state),
-  groups: getGroupsList(state),
+  groups: getCurrentUserGroupsWithManagePages(state),
   pageTemplates: getPageTemplatesList(state),
   pages: getPageTreePages(state),
   charsets: getCharsets(state),
@@ -52,7 +56,7 @@ export const mapDispatchToProps = (dispatch, ownProps) => ({
     }).catch(() => {}),
   onWillMount: ({ pageCode }) => {
     dispatch(fetchLanguages({ page: 1, pageSize: 0 }));
-    dispatch(fetchGroups({ page: 1, pageSize: 0 }));
+    dispatch(fetchCurrentUserGroups({ page: 1, pageSize: 0 }));
     dispatch(fetchPageTemplates({ page: 1, pageSize: 0 }));
     dispatch(fetchPageForm(pageCode));
   },
