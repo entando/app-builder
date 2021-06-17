6a5d4bfa3543423d3463aab5dc762bf436e37314
Jeff Go <j.go@entando.com>
2021-06-17 14:25:32 +0800

    ENG-2244 commands refactoring

--- test/cypress/integration/components-micro-frontends-and-widgets.spec.js
+++ test/cypress/integration/components-micro-frontends-and-widgets.spec.js
@@ -10,6 +10,12 @@ import {
 import { TEST_ID_PAGE_DESIGNER } from '../../../src/ui/test-const/page-designer-test-const';
 
 const SAMPLE_BASIC_WIDGET_ID = 'my_widget';
+const SAMPLE_DUPE_WIDGET_CODE = 'mio_widget';
+
+const PAGE = {
+  title: 'My Homepage',
+  code: 'my_homepage',
+};
 
 describe('Microfrontends and Widgets', () => {
   beforeEach(() => {
@@ -23,7 +29,7 @@ describe('Microfrontends and Widgets', () => {
 
   describe('Widgets CRUD', () => {
     it('Adding a basic widget with icon', () => {
-      cy.openPageFromMenu(['Components', 'Micro Frontends & Widgets']);
+      cy.openPageFromMenu(['Components', 'MFE & Widgets']);
       cy.validateUrlChanged('/widget');
       cy.getByTestId(TEST_ID_COMPONENT_LIST_BUTTON).contains('Add').click();
       cy.validateUrlChanged('/widget/add');
@@ -36,7 +42,7 @@ describe('Microfrontends and Widgets', () => {
     });
 
     it('Editing the widget we just made', () => {
-      cy.openPageFromMenu(['Components', 'Micro Frontends & Widgets']);
+      cy.openPageFromMenu(['Components', 'MFE & Widgets']);
       cy.openTableActionsByTestId(SAMPLE_BASIC_WIDGET_ID);
       cy.getVisibleActionItemByClass(EDIT_ACTION_CLASSNAME).click();
       cy.wait(500);
@@ -47,13 +53,13 @@ describe('Microfrontends and Widgets', () => {
       cy.get('.FragmentForm__dropdown [role=menu]').contains('Save and continue').click();
       cy.location('pathname').should('not.eq', '/widget');
       cy.validateUrlChanged(`/widget/edit/${SAMPLE_BASIC_WIDGET_ID}`);
-      cy.openPageFromMenu(['Components', 'Micro Frontends & Widgets']);
+      cy.openPageFromMenu(['Components', 'MFE & Widgets']);
       cy.wait(500);
       cy.get('table').should('contain', 'Your Widget');
     });
 
     it('Delete the widget we just made', () => {
-      cy.openPageFromMenu(['Components', 'Micro Frontends & Widgets']);
+      cy.openPageFromMenu(['Components', 'MFE & Widgets']);
       cy.openTableActionsByTestId(SAMPLE_BASIC_WIDGET_ID);
       cy.getVisibleActionItemByClass(DELETE_ACTION_CLASSNAME).click();
       cy.getModalDialogByTitle('Delete').should('be.visible');
@@ -63,20 +69,21 @@ describe('Microfrontends and Widgets', () => {
   });
 
   describe('Widget Usage for CMS Content Widget', () => {
-    const PAGE = 'My Homepage';
-    const PAGE_CODE = 'my_homepage';
-    const WIDGET_CODE = 'content_viewer';
-    it('Open Widget Settings', () => {
-      cy.openPageFromMenu(['Pages', 'Designer']);
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.CONFIG_TABS).contains('Page Tree').click();
+    const WIDGET_FRAME = {
+      frameName: 'Frame 3',
+      frameNum: 6,
+      widgetCode: 'content_viewer',
+      widgetName: 'Content',
+    };
+
+    it('Basic add with widget settings', () => {
+      cy.openPageViaPageDesigner(PAGE);
       cy.wait(500);
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.PAGE_TREE).contains(PAGE).click();
-      cy.log('Select the widget');
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.CONFIG_TABS).contains('Widgets').click();
-      cy.validateUrlChanged(`/page/configuration/${PAGE_CODE}`);
-      cy.log('Add the widget to the page in Frame 3');
-      cy.addWidgetToFrame('Content', 'Frame 3');
-      cy.validateUrlChanged(`/widget/config/${WIDGET_CODE}/page/${PAGE_CODE}/frame/6`);
+
+      cy.log(`Add the widget to the page in ${WIDGET_FRAME.frameName}`);
+      cy.addWidgetToFrame(WIDGET_FRAME.widgetName, WIDGET_FRAME.frameName);
+
+      cy.validateUrlChanged(`/widget/config/${WIDGET_FRAME.widgetCode}/page/${PAGE.code}/frame/${WIDGET_FRAME.frameNum}`);
       cy.getByTestId(TEST_ID_PAGE_DESIGNER.WIDGET_CONFIG).contains('Add existing content').click();
       cy.getModalDialogByTitle('Select one content item').should('be.visible');
       cy.get('#selectTCL6').click();
@@ -85,42 +92,133 @@ describe('Microfrontends and Widgets', () => {
       cy.get('.modal-dialog').should('not.exist');
       cy.getByTestId(TEST_ID_PAGE_DESIGNER.WIDGET_CONFIG).contains('Save').click();
       cy.wait(500);
-      cy.getPageStatus().should('match', new RegExp('^Published, with pending changes$'));
+
+      cy.getPageStatus().should('match', /^Published, with pending changes$/);
       cy.publishPageClick();
-      cy.getPageStatus().should('match', new RegExp('^Published$'));
+      cy.getPageStatus().should('match', /^Published$/);
     });
 
     it('Open Widget Details from the widget dropped', () => {
-      cy.openPageFromMenu(['Pages', 'Designer']);
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.CONFIG_TABS).contains('Page Tree').click();
+      cy.openPageViaPageDesigner(PAGE);
       cy.wait(500);
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.PAGE_TREE).contains(PAGE).click();
-      cy.log('Select the widget');
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.CONFIG_TABS).contains('Widgets').click();
-      cy.wait(500);
-      cy.openPageWidgetDetailsByFrame('Frame 3');
-      cy.validateUrlChanged(`/widget/detail/${WIDGET_CODE}`);
+
+      cy.openPageWidgetDetailsByFrame(WIDGET_FRAME.frameName);
+      cy.validateUrlChanged(`/widget/detail/${WIDGET_FRAME.widgetCode}`);
     });
 
     it('Save As Widget', () => {
-      cy.openPageFromMenu(['Pages', 'Designer']);
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.CONFIG_TABS).contains('Page Tree').click();
+      cy.openPageViaPageDesigner(PAGE);
       cy.wait(500);
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.PAGE_TREE).contains(PAGE).click();
-      cy.log('Select the widget');
-      cy.getByTestId(TEST_ID_PAGE_DESIGNER.CONFIG_TABS).contains('Widgets').click();
-      cy.wait(500);
-      cy.openSaveAsWidgetWithFrame('Frame 3');
-      cy.validateUrlChanged(`/page/${PAGE_CODE}/clone/6/widget/${WIDGET_CODE}/viewerConfig`);
-      cy.fillUpWidgetForm('Mio Widget', 'mio_widget', '', 'Free Access');
+
+      cy.openSaveAsWidgetWithFrame(WIDGET_FRAME.frameName);
+
+      cy.validateUrlChanged(`/page/${PAGE.code}/clone/${WIDGET_FRAME.frameNum}/widget/${WIDGET_FRAME.widgetCode}/viewerConfig`);
+      cy.fillUpWidgetForm('Mio Widget', SAMPLE_DUPE_WIDGET_CODE, '', 'Free Access');
       cy.get('form').contains('Parent Type').should('exist');
       cy.get('form').contains('Configuration *').should('exist');
       cy.get('form').contains('Configuration *').click();
       cy.wait(500);
       cy.get('form').contains('Change content').should('exist');
       cy.get(`.${WIDGET_FORM_HEADER_CLASSNAME}`).contains('Save and Replace').click();
+
       cy.wait(4500);
-      cy.validateUrlChanged(`/page/configuration/${PAGE_CODE}`);
+      cy.validateUrlChanged(`/page/configuration/${PAGE.code}`);
+      cy.getPageStatus().should('match', /^Published, with pending changes$/);
+      cy.publishPageClick();
+      cy.wait(500);
+      cy.getPageStatus().should('match', /^Published$/);
+    });
+
+    it('Test widget cleanup', () => {
+      cy.openPageViaPageDesigner(PAGE);
+      cy.wait(500);
+      cy.deletePageWidgetByFrame(WIDGET_FRAME.frameName);
+      cy.publishPageClick();
+      cy.openPageFromMenu(['Components', 'MFE & Widgets']);
+      cy.openTableActionsByTestId(SAMPLE_DUPE_WIDGET_CODE);
+      cy.getVisibleActionItemByClass(DELETE_ACTION_CLASSNAME).click();
+      cy.getModalDialogByTitle('Delete').should('be.visible');
+      cy.getButtonByText('Delete').click();
+      cy.get('table').should('not.contain', SAMPLE_DUPE_WIDGET_CODE);
+    });
+  });
+
+  describe('Widget Usage for CMS Content List Widget', () => {
+    const WIDGET_FRAME = {
+      frameName: 'Frame 4',
+      frameNum: 7,
+      widgetCode: 'row_content_viewer_list',
+      widgetName: 'Content List',
+    };
+
+    it('Basic add with widget settings', () => {
+      cy.openPageViaPageDesigner(PAGE);
+      cy.wait(500);
+
+      cy.log(`Add the widget to the page in ${WIDGET_FRAME.frameName}`);
+      cy.addWidgetToFrame(WIDGET_FRAME.widgetName, WIDGET_FRAME.frameName);
+
+      cy.validateUrlChanged(`/widget/config/${WIDGET_FRAME.widgetCode}/page/${PAGE.code}/frame/${WIDGET_FRAME.frameNum}`);
+      cy.wait(5000);
+      cy.get('.Contents__body td').contains('Sample - About Us')
+        .siblings().contains('Add')
+        .click();
+      cy.get('.Contents__body td').contains('Sample Banner')
+        .siblings().contains('Add')
+        .click();
+      cy.wait(500);
+      cy.getByName('contents[0].modelId').select('2-column-content');
+      cy.getByName('contents[1].modelId').select('Banner - Text, Image, CTA');
+      cy.getByTestId(TEST_ID_PAGE_DESIGNER.WIDGET_CONFIG).contains('Save').click();
+
+      cy.wait(500);
+      cy.getPageStatus().should('match', /^Published, with pending changes$/);
+      cy.publishPageClick();
+      cy.getPageStatus().should('match', /^Published$/);
+    });
+
+    it('Open Widget Details from the widget dropped', () => {
+      cy.openPageViaPageDesigner(PAGE);
+      cy.wait(500);
+
+      cy.openPageWidgetDetailsByFrame(WIDGET_FRAME.frameName);
+      cy.validateUrlChanged(`/widget/detail/${WIDGET_FRAME.widgetCode}`);
+    });
+
+    it('Save As Widget', () => {
+      cy.openPageViaPageDesigner(PAGE);
+      cy.wait(500);
+
+      cy.openSaveAsWidgetWithFrame(WIDGET_FRAME.frameName);
+
+      cy.validateUrlChanged(`/page/${PAGE.code}/clone/${WIDGET_FRAME.frameNum}/widget/${WIDGET_FRAME.widgetCode}/rowListViewerConfig`);
+      cy.fillUpWidgetForm('Mio Widget', SAMPLE_DUPE_WIDGET_CODE, '', 'Free Access');
+      cy.get('form').contains('Parent Type').should('exist');
+      cy.get('form').contains('Configuration *').should('exist');
+      cy.get('form').contains('Configuration *').click();
+      cy.wait(500);
+      cy.get('form').contains('Sample Banner').should('exist');
+      cy.get(`.${WIDGET_FORM_HEADER_CLASSNAME}`).contains('Save and Replace').click();
+
+      cy.wait(4500);
+      cy.validateUrlChanged(`/page/configuration/${PAGE.code}`);
+      cy.getPageStatus().should('match', /^Published, with pending changes$/);
+      cy.publishPageClick();
+      cy.wait(500);
+      cy.getPageStatus().should('match', /^Published$/);
+    });
+
+    it('Test widget cleanup', () => {
+      cy.openPageViaPageDesigner(PAGE);
+      cy.wait(500);
+      cy.deletePageWidgetByFrame(WIDGET_FRAME.frameName);
+      cy.publishPageClick();
+      cy.openPageFromMenu(['Components', 'MFE & Widgets']);
+      cy.openTableActionsByTestId(SAMPLE_DUPE_WIDGET_CODE);
+      cy.getVisibleActionItemByClass(DELETE_ACTION_CLASSNAME).click();
+      cy.getModalDialogByTitle('Delete').should('be.visible');
+      cy.getButtonByText('Delete').click();
+      cy.get('table').should('not.contain', SAMPLE_DUPE_WIDGET_CODE);
     });
   });
 });
