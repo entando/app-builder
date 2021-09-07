be57a707178601ef2a2fd09c1fda1c0a87b89acb
Guilherme <guilherme.r89@gmail.com>
2021-09-07 11:29:20 -0300

    adding test to owner group field

--- test/ui/pages/common/PageForm.test.js
+++ test/ui/pages/common/PageForm.test.js
@@ -210,4 +210,29 @@ describe('PageForm', () => {
       expect(component.find('.PageForm__save-and-configure-btn').prop('disabled')).toBe(false);
     });
   });
+
+  describe('on edit mode', () => {
+    let component;
+    beforeEach(() => {
+      component = shallow((
+        <PageFormBody
+          onSubmit={ON_SUBMIT}
+          handleSubmit={HANDLE_SUBMIT}
+          languages={LANGUAGES}
+          groups={GROUPS}
+          allGroups={GROUPS}
+          pageTemplates={PAGE_TEMPLATES}
+          contentTypes={CONTENT_TYPES}
+          charsets={CHARSETS}
+          selectedJoinGroups={[]}
+          invalid={false}
+          intl={mockIntl}
+          mode="edit"
+        />
+      ));
+    });
+    it('Owner group should be disabled', () => {
+      expect(component.find('Field[name="ownerGroup"]').prop('disabled')).toBe(true);
+    });
+  });
 });
