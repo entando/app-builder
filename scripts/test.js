76b62859d8d8c11e9b376a34ff583a38aa90e7f7
Walter Caffiero <w.caffiero@entando.com>
2021-10-18 10:04:17 +0200

    ENG-2655 Migrate to the GitHub pipelines (+ENG-2392)
    Migration to version 1.1.0 of the entando-pipelines
    Also includes ENG-2392 for consistency check between versions declared
    in the project file, the lock file and the dockerfile

--- scripts/test.js
+++ scripts/test.js
@@ -23,8 +23,8 @@ const argv = process.argv.slice(2);
 if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
-if (process.env.JX) {
-  argv.push('--maxWorkers=3');
+if (process.env.MAX_WORKERS) {
+  argv.push(`--maxWorkers=${process.env.ENTANDO_TEST_MAX_WORKERS}`);
 }
 
 jest.run(argv);
