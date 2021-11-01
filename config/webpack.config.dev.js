8fc20388b255901f002155bac596359939d8d263
Irakli Chalagashvili <chalagashvili5@gmail.com>
2021-11-01 18:08:44 +0400

    fix some config to make it work

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -19,6 +19,7 @@ const publicPath = '/dbg-app-builder/';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+// const publicUrl = '/dbg-app-builder/';
 const publicUrl = '/dbg-app-builder/';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
