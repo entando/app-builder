4c5bc6a5c8db271108d8c70c21fe20e1b2c49023
Jeff Go <j.go@entando.com>
2022-01-13 16:59:31 +0800

    ENG-3110 point cms links to admin console

--- src/helpers/urlUtils.js
+++ src/helpers/urlUtils.js
@@ -1,3 +1,3 @@
 import { get } from 'lodash';
 // eslint-disable-next-line import/prefer-default-export
-export const adminConsoleUrl = url => `${get(process.env, 'REACT_APP_DOMAIN', '')}/${url}`;
+export const adminConsoleUrl = url => `${get(process.env, 'DOMAIN', '')}/${url}`;
