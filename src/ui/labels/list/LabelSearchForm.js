cf745254b1cd9be4507922da742dac4cfaa1ddd5
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-01-24 14:04:15 +0400

    formik/ENG-3121 replace old label search form with formik

--- src/ui/labels/list/LabelSearchForm.js
+++ src/ui/labels/list/LabelSearchForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { reduxForm, Field } from 'redux-form';
+import { Formik, Form, Field } from 'formik';
 import { FormattedMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
 import { Row, Col, FormGroup, Button } from 'patternfly-react';
 
@@ -11,55 +11,59 @@ const msgs = defineMessages({
   },
 });
 
-export const LabelSearchFormBody = ({ intl, handleSubmit }) => {
-  const onSubmit = (ev) => {
-    ev.preventDefault();
-    handleSubmit();
+export const LabelSearchFormBody = ({ intl, onSubmit }) => {
+  const handleSubmit = (values) => {
+    onSubmit(values);
   };
 
   return (
-    <form onSubmit={onSubmit} className="LabelSearchForm form-horizontal well">
-      <h3><FormattedMessage id="label.searchForm.title" /></h3>
-      <FormGroup>
-        <Row>
-          <label className="control-label col-xs-2" htmlFor="username">
-            <FormattedMessage id="label.searchForm.code" />
-          </label>
-          <Col xs={9}>
-            <Field
-              id="text"
-              component="input"
-              className="LabelSearchForm__text-field form-control"
-              name="key"
-              placeholder={intl.formatMessage(msgs.searchFormCode)}
-            />
-          </Col>
-        </Row>
-      </FormGroup>
-      <FormGroup>
-        <Row>
-          <Col xs={11}>
-            <Button
-              type="submit"
-              bsStyle="primary"
-              className="pull-right"
-            >
-              <FormattedMessage id="app.search" />
-            </Button>
-          </Col>
-        </Row>
-      </FormGroup>
-    </form>
+    <Formik
+      initialValues={{ key: '' }}
+      onSubmit={handleSubmit}
+    >
+      {
+        () => (
+          <Form className="LabelSearchForm form-horizontal well">
+            <h3><FormattedMessage id="label.searchForm.title" /></h3>
+            <FormGroup>
+              <Row>
+                <label className="control-label col-xs-2" htmlFor="username">
+                  <FormattedMessage id="label.searchForm.code" />
+                </label>
+                <Col xs={9}>
+                  <Field
+                    id="text"
+                    component="input"
+                    className="LabelSearchForm__text-field form-control"
+                    name="key"
+                    placeholder={intl.formatMessage(msgs.searchFormCode)}
+                  />
+                </Col>
+              </Row>
+            </FormGroup>
+            <FormGroup>
+              <Row>
+                <Col xs={11}>
+                  <Button
+                    type="submit"
+                    bsStyle="primary"
+                    className="pull-right"
+                  >
+                    <FormattedMessage id="app.search" />
+                  </Button>
+                </Col>
+              </Row>
+            </FormGroup>
+          </Form>
+        )
+      }
+    </Formik>
   );
 };
 
 LabelSearchFormBody.propTypes = {
   intl: intlShape.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
 
-const LabelSearchForm = reduxForm({
-  form: 'labelSearch',
-})(LabelSearchFormBody);
-
-export default injectIntl(LabelSearchForm);
+export default injectIntl(LabelSearchFormBody);
--- test/ui/labels/list/LabelSearchForm.test.js
+++ test/ui/labels/list/LabelSearchForm.test.js
@@ -1,44 +1,45 @@
 import React from 'react';
-import 'test/enzyme-init';
-import { mount } from 'enzyme';
+import '@testing-library/jest-dom/extend-expect';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import LabelSearchForm from 'ui/labels/list/LabelSearchForm';
 import { mockRenderWithIntlAndStore } from 'test/legacyTestUtils';
 
 const handleSubmit = jest.fn();
 
 jest.unmock('react-redux');
-jest.unmock('redux-form');
 
 describe('LabelSearchFormBody', () => {
   let labelSearchForm;
 
   describe('basic render tests', () => {
     beforeEach(() => {
-      labelSearchForm = mount(mockRenderWithIntlAndStore(<LabelSearchForm
-        handleSubmit={handleSubmit}
+      labelSearchForm = render(mockRenderWithIntlAndStore(<LabelSearchForm
+        onSubmit={handleSubmit}
       />));
     });
     it('root component renders without crashing', () => {
-      expect(labelSearchForm.exists()).toEqual(true);
+      expect(labelSearchForm).toBeDefined();
     });
 
     it('root component renders language field', () => {
-      const text = labelSearchForm.find('.LabelSearchForm__text-field');
-      expect(text.exists()).toEqual(true);
+      expect(labelSearchForm.getByRole('textbox')).toBeDefined();
     });
   });
 
   describe('event handlers test', () => {
-    const preventDefault = jest.fn();
-    beforeEach(() => {
-      labelSearchForm = mount(mockRenderWithIntlAndStore(<LabelSearchForm
-        handleSubmit={handleSubmit}
+    it('on form submit calls handleSubmit', async () => {
+      const onSubmit = jest.fn();
+      const form = render(mockRenderWithIntlAndStore(<LabelSearchForm
+        onSubmit={onSubmit}
       />));
-    });
-
-    it('on form submit calls handleSubmit', () => {
-      labelSearchForm.find('form').simulate('submit', { preventDefault });
-      expect(handleSubmit).toHaveBeenCalled();
+      const testValue = 'Test Label';
+      userEvent.type(form.getByRole('textbox'), testValue);
+      fireEvent.click(form.getByText('Search'));
+      await waitFor(() => {
+        expect(onSubmit).toHaveBeenCalledWith({ key: testValue });
+      });
     });
   });
 });
