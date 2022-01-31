16cd5bf4ceebcafe383bddbe19e4b46964254b75
Rax Canaan Layumas <raxkaynan@gmail.com>
2022-01-31 17:41:50 +0800

    ENG-3130 Update email sender form tests

--- test/ui/email-config/AddEmailSenderFormContainer.test.js
+++ test/ui/email-config/AddEmailSenderFormContainer.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import * as reactRedux from 'react-redux';
 
-import { renderWithIntlAndState, setupForm } from 'test/testUtils';
-import userEvent from '@testing-library/user-event';
+import { renderWithState } from 'test/testUtils';
 import AddEmailSenderFormContainer from 'ui/email-config/AddEmailSenderFormContainer';
+import EmailSenderForm from 'ui/email-config/EmailSenderForm';
 
 jest.unmock('react-redux');
-jest.unmock('redux-form');
 
 jest.mock('state/email-config/actions', () => ({
   addEmailSender: jest.fn(payload => ({ type: 'addEmailSender_test', payload })),
@@ -18,51 +16,41 @@ const useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
 const mockDispatch = jest.fn();
 useDispatchSpy.mockReturnValue(mockDispatch);
 
+jest.mock('ui/email-config/EmailSenderForm', () => jest.fn(mockProps => (
+  <div>
+    <button onClick={() => mockProps.onSubmit({})}>onSubmit</button>
+  </div>
+)));
+
+const setupAddEmailSenderFormContainer = () => {
+  const utils = renderWithState(<AddEmailSenderFormContainer />);
+  const simulateSubmit = () => utils.getByText('onSubmit').click();
+
+  return {
+    ...utils,
+    simulateSubmit,
+  };
+};
+
 describe('AddEmailSenderFormContainer', () => {
-  it('should render the correct heading', () => {
-    renderWithIntlAndState(<AddEmailSenderFormContainer />);
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('New Sender');
+  afterEach(() => {
+    mockDispatch.mockClear();
   });
 
-  describe('form', () => {
-    const setupEmailSenderForm = () => {
-      const formValues = {
-        code: 'testcode',
-        email: 'testemail@test.com',
-      };
-      return setupForm('emailSender', formValues, renderWithIntlAndState, <AddEmailSenderFormContainer />);
-    };
+  it('passes initialValues to EmailSenderForm with the correct values', () => {
+    setupAddEmailSenderFormContainer();
 
-    it('should render code and email fields', () => {
-      const { formView, formValues } = setupEmailSenderForm();
-      expect(formView.getByRole('textbox', { name: 'Code' })).toHaveDisplayValue(formValues.code);
-      expect(formView.getByRole('textbox', { name: 'Email' })).toHaveDisplayValue(formValues.email);
-    });
+    expect(EmailSenderForm).toHaveBeenCalledWith(expect.objectContaining({
+      initialValues: { code: '', email: '' },
+    }), {});
+  });
 
-    it('should render "Save" button that calls the correct action when clicked', () => {
-      const { formView, formValues } = setupEmailSenderForm();
-      const saveBtn = formView.getByRole('button', { name: 'Save' });
-      expect(saveBtn).toHaveAttribute('type', 'submit');
-      mockDispatch.mockClear();
-      userEvent.click(saveBtn);
-      expect(mockDispatch).toHaveBeenCalledTimes(1);
-      expect(mockDispatch).toHaveBeenCalledWith({ type: 'addEmailSender_test', payload: formValues });
-    });
+  it('calls the correct email sender action when EmailSenderForm is submitted', () => {
+    const { simulateSubmit } = setupAddEmailSenderFormContainer();
 
-    it('should render a disabled "Save" button when code field is empty', () => {
-      const { formView } = setupEmailSenderForm();
-      const codeField = formView.getByRole('textbox', { name: 'Code' });
-      fireEvent.change(codeField, { target: { value: '' } });
-      const saveBtn = formView.getByRole('button', { name: 'Save' });
-      expect(saveBtn).toBeDisabled();
-    });
+    simulateSubmit();
 
-    it('should render a disabled "Save" button when email field is empty', () => {
-      const { formView } = setupEmailSenderForm();
-      const emailField = formView.getByRole('textbox', { name: 'Email' });
-      fireEvent.change(emailField, { target: { value: '' } });
-      const saveBtn = formView.getByRole('button', { name: 'Save' });
-      expect(saveBtn).toBeDisabled();
-    });
+    expect(mockDispatch).toHaveBeenCalledTimes(1);
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'addEmailSender_test', payload: {} });
   });
 });
--- test/ui/email-config/EditEmailSenderFormContainer.test.js
+++ test/ui/email-config/EditEmailSenderFormContainer.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import * as reactRedux from 'react-redux';
 
-import { renderWithIntlRouterState, setupForm } from 'test/testUtils';
-import userEvent from '@testing-library/user-event';
+import { renderWithIntlRouterState } from 'test/testUtils';
 import EditEmailSenderFormContainer from 'ui/email-config/EditEmailSenderFormContainer';
+import EmailSenderForm from 'ui/email-config/EmailSenderForm';
+import { MOCK_EMAIL_SENDER } from 'test/mocks/emailConfig';
 
 jest.unmock('react-redux');
-jest.unmock('redux-form');
 
 jest.mock('state/email-config/actions', () => ({
   fetchEmailSender: jest.fn(payload => ({ type: 'fetchEmailSender_test', payload })),
@@ -19,68 +18,53 @@ const useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
 const mockDispatch = jest.fn();
 useDispatchSpy.mockReturnValue(mockDispatch);
 
+jest.mock('ui/email-config/EmailSenderForm', () => jest.fn(mockProps => (
+  <div>
+    <button onClick={() => mockProps.onSubmit({})}>onSubmit</button>
+  </div>
+)));
+
+const setupEditEmailSenderFormContainer = (state = {
+  emailConfig: { selectedSender: MOCK_EMAIL_SENDER },
+}) => {
+  const utils = renderWithIntlRouterState(<EditEmailSenderFormContainer />, {
+    state, initialRoute: '/email-config/senders/edit/testcode', path: '/email-config/senders/edit/:code',
+  });
+  const simulateSubmit = () => utils.getByText('onSubmit').click();
+
+  return {
+    ...utils,
+    simulateSubmit,
+  };
+};
+
 describe('EditEmailSenderFormContainer', () => {
   afterEach(() => {
     mockDispatch.mockClear();
   });
 
-  const testCode = 'testcode';
+  it('fetches email sender data when form mounts', () => {
+    setupEditEmailSenderFormContainer();
 
-  it('should fetch the relevant data on initial render', () => {
-    const ROUTE_EMAIL_CONFIG_SENDERS_EDIT_TESTCODE = `/email-config/senders/edit/${testCode}`;
-    renderWithIntlRouterState(<EditEmailSenderFormContainer />, {
-      initialRoute: ROUTE_EMAIL_CONFIG_SENDERS_EDIT_TESTCODE,
-      path: '/email-config/senders/edit/:code',
-    });
     expect(mockDispatch).toHaveBeenCalledTimes(1);
-    expect(mockDispatch).toHaveBeenCalledWith({ type: 'fetchEmailSender_test', payload: testCode });
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'fetchEmailSender_test', payload: 'testcode' });
   });
 
-  it('should render the correct heading', () => {
-    renderWithIntlRouterState(<EditEmailSenderFormContainer />);
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Edit Sender');
+  it('passes initialValues to EmailSenderForm with the correct values', () => {
+    setupEditEmailSenderFormContainer();
+
+    expect(EmailSenderForm).toHaveBeenCalledWith(expect.objectContaining({
+      initialValues: MOCK_EMAIL_SENDER,
+    }), {});
   });
 
-  describe('form', () => {
-    const setupEmailSenderForm = () => {
-      const formValues = {
-        code: 'testcode',
-        email: 'testemail@test.com',
-      };
-      return setupForm('emailSender', formValues, renderWithIntlRouterState, <EditEmailSenderFormContainer />);
-    };
+  it('calls the correct email sender action when EmailSenderForm is submitted', () => {
+    const { simulateSubmit } = setupEditEmailSenderFormContainer();
 
-    it('should render code and email fields with code field disabled', () => {
-      const { formView, formValues } = setupEmailSenderForm();
-      expect(formView.getByRole('textbox', { name: 'Code' })).toHaveDisplayValue(formValues.code);
-      expect(formView.getByRole('textbox', { name: 'Email' })).toHaveDisplayValue(formValues.email);
-      expect(formView.getByRole('textbox', { name: 'Code' })).toBeDisabled();
-    });
+    simulateSubmit();
 
-    it('should render "Save" button that calls the correct action when clicked', () => {
-      const { formView, formValues } = setupEmailSenderForm();
-      const saveBtn = formView.getByRole('button', { name: 'Save' });
-      expect(saveBtn).toHaveAttribute('type', 'submit');
-      mockDispatch.mockClear();
-      userEvent.click(saveBtn);
-      expect(mockDispatch).toHaveBeenCalledTimes(1);
-      expect(mockDispatch).toHaveBeenCalledWith({ type: 'updateEmailSender_test', payload: formValues });
-    });
-
-    it('should render a disabled "Save" button when code field is empty', () => {
-      const { formView } = setupEmailSenderForm();
-      const codeField = formView.getByRole('textbox', { name: 'Code' });
-      fireEvent.change(codeField, { target: { value: '' } });
-      const saveBtn = formView.getByRole('button', { name: 'Save' });
-      expect(saveBtn).toBeDisabled();
-    });
-
-    it('should render a disabled "Save" button when email field is empty', () => {
-      const { formView } = setupEmailSenderForm();
-      const emailField = formView.getByRole('textbox', { name: 'Email' });
-      fireEvent.change(emailField, { target: { value: '' } });
-      const saveBtn = formView.getByRole('button', { name: 'Save' });
-      expect(saveBtn).toBeDisabled();
-    });
+    expect(mockDispatch).toHaveBeenCalledTimes(2);
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'updateEmailSender_test', payload: {} });
   });
 });
+
