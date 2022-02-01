40c112771dfb64c11724e886639033b12b1e4a93
Guilherme <guilherme.r89@gmail.com>
2022-02-01 10:03:09 -0300

    re-implement smtp email config form on formik

--- src/state/email-config/reducer.js
+++ src/state/email-config/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import { REMOVE_EMAIL_SENDER, SET_EMAIL_SENDERS } from 'state/email-config/types';
+import { REMOVE_EMAIL_SENDER, SET_EMAIL_SENDERS, SET_SMTP_SERVER } from 'state/email-config/types';
 
 const senders = (state = [], action = {}) => {
   switch (action.type) {
@@ -13,6 +13,28 @@ const senders = (state = [], action = {}) => {
   }
 };
 
+export const initialState = {
+  active: false,
+  debugMode: false,
+  host: '',
+  port: '',
+  protocol: '',
+  checkServerIdentity: false,
+  timeout: '',
+  username: '',
+  password: '',
+};
+
+const smtpServer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case SET_SMTP_SERVER:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
 export default combineReducers({
   senders,
+  smtpServer,
 });
--- src/state/email-config/types.js
+++ src/state/email-config/types.js
@@ -1,2 +1,3 @@
 export const SET_EMAIL_SENDERS = 'emailConfig/set-email-sender-list';
 export const REMOVE_EMAIL_SENDER = 'emailConfig/remove-email-sender';
+export const SET_SMTP_SERVER = 'emailConfig/set-smtp-server';
