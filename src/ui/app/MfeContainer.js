b1d8419e69b604da2a64a129e019824589886df5
Marco Corradetti <m.corradetti@entando.com>
2022-05-24 13:01:31 +0200

    add appBuilderRouter to the MfeContainer

--- src/ui/app/MfeContainer.js
+++ src/ui/app/MfeContainer.js
@@ -1,17 +1,33 @@
 import { renderMicrofrontend } from 'helpers/microfrontends';
+import { withRouter } from 'react-router-dom';
 import useMfe from 'hooks/useMfe';
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const MfeContainer = ({ id }) => {
+const MfeContainer = ({ id, history }) => {
   const [assetLoading, mfe] = useMfe(id);
 
+  useEffect(() => {
+    if (!window.appBuilderRouter) {
+      window.appBuilderRouter = history;
+    }
+  }, [history]);
+
+  const params = {
+    config: {
+      api: mfe.api,
+    },
+  };
+
   return assetLoading.length
-    ? <div>Loading...</div> : renderMicrofrontend(mfe.customElement, { api: mfe.api });
+    ? <div>Loading...</div>
+    : renderMicrofrontend(mfe.customElement, params);
 };
 
 MfeContainer.propTypes = {
   id: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  history: PropTypes.object.isRequired,
 };
 
-export default MfeContainer;
+export default withRouter(MfeContainer);
