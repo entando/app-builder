a8f5f16b8f430cb95f8133b7185063831b452cf7
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-06-30 12:07:02 +0400

    ENG-3717 empty commit for image generation

--- src/ui/app/NoAccessPageContainer.js
+++ src/ui/app/NoAccessPageContainer.js
@@ -2,7 +2,6 @@
 import { routeConverter } from '@entando/utils';
 import { connect } from 'react-redux';
 import NoAccess from 'ui/app/NoAccess';
-
 import { ROUTE_DASHBOARD } from 'app-init/router';
 
 const mapDispatchToProps = (dispatch, { history }) => ({
