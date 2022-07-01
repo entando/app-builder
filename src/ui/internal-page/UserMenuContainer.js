0ca5dbee0db7f8a049eddd7a3e733edfc7da2424
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-07-01 12:59:05 +0400

    ENG-3908 fix loop empty line

--- src/ui/internal-page/UserMenuContainer.js
+++ src/ui/internal-page/UserMenuContainer.js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 import { logoutUser, getUsername } from '@entando/apimanager';
 
 import UserMenu from 'ui/internal-page/UserMenu';
-
 import { dismissedWizardKey } from 'ui/app-tour/constant';
 
 export const mapStateToProps = state => ({
