5c59a2752c0eb353cb20dccc30ceac64f9d6fcc6
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-07-19 17:10:30 +0400

    ENG-3949 change mfe route path generation

--- test/helpers/urlUtils.test.js
+++ test/helpers/urlUtils.test.js
@@ -5,27 +5,27 @@ describe('Verify corectness of mfe routes generation', () => {
   it('should generate correct routes for mfe if id is not available', () => {
     const mfe = LIST_MFE_RESPONSE_OK[6];
     expect(generateMfeRoutes([mfe])).toEqual([
-      { route: '/example-mfe-4-group/example-mfe-4-code/', id: 'id-example-mfe-4-group-example-mfe-4-code-/-example-mfe-4-widget-code' }]);
+      { route: '/example-mfe-4-code/', id: 'id-example-mfe-4-group-example-mfe-4-code-/-example-mfe-4-widget-code' }]);
   });
 
   it('should generate correct routes for mfe if id has spaces inside', () => {
     const mfe = LIST_MFE_RESPONSE_OK.find(item => item.id === 'example   mfe    2 with spaces');
     expect(generateMfeRoutes([mfe])).toEqual([
-      { route: '/example-mfe-2/example-mfe-2/', id: 'example-mfe-2-with-spaces' }]);
+      { route: '/example-mfe-2/', id: 'example-mfe-2-with-spaces' }]);
   });
 
   it('should generate correct routes for mfe if id is available', () => {
     const mfe = LIST_MFE_RESPONSE_OK.find(item => !!item.id);
     expect(generateMfeRoutes([mfe])).toEqual([
-      { route: '/layout-core-bundle/layout-core-bundle-id/route1', id: '1a793414-d773-42ef-922e-00acafa43aa9' },
-      { route: '/layout-core-bundle/layout-core-bundle-id/route2', id: '1a793414-d773-42ef-922e-00acafa43aa9' }]);
+      { route: '/layout-core-bundle-id/route1', id: '1a793414-d773-42ef-922e-00acafa43aa9' },
+      { route: '/layout-core-bundle-id/route2', id: '1a793414-d773-42ef-922e-00acafa43aa9' }]);
   });
 
   it('should generate correct routes for mfe not depending if path starts with "/" or not', () => {
     const mfe = { ...LIST_MFE_RESPONSE_OK[0] };
     expect(generateMfeRoutes([mfe])).toEqual([
-      { route: '/layout-core-bundle/layout-core-bundle-id/route1', id: '1a793414-d773-42ef-922e-00acafa43aa9' },
-      { route: '/layout-core-bundle/layout-core-bundle-id/route2', id: '1a793414-d773-42ef-922e-00acafa43aa9' }]);
+      { route: '/layout-core-bundle-id/route1', id: '1a793414-d773-42ef-922e-00acafa43aa9' },
+      { route: '/layout-core-bundle-id/route2', id: '1a793414-d773-42ef-922e-00acafa43aa9' }]);
   });
 
   it('should return empty array if no paths available inside mfe object', () => {
