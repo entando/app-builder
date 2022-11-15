3ed04dbc383820f206e1e87e68b56739f2d3cd9f
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-11-15 08:59:36 +0400

    ENG-4272 implement page reload once component finishes installing

--- src/state/component-repository/components/actions.js
+++ src/state/component-repository/components/actions.js
@@ -231,6 +231,8 @@ export const pollECRComponentInstallStatus = (componentCode, stepFunction) => di
           dispatch(finishComponentInstallation(componentCode, res.payload));
           dispatch(fetchSelectedBundleStatusWithCode(componentCode));
           dispatch(fetchMfeConfigList());
+          // refresh the page
+          window.location.reload();
         } else {
           dispatch(componentInstallationFailed(componentCode));
           if (res.payload.installErrorMessage) {
