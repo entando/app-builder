e8723fae0345953bf6f5b9010ac422e1f34d612b
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-11-29 14:30:11 +0400

    ENG-3420 fix current page deletion from designer

--- src/ui/pages/common/DeletePageModalContainer.js
+++ src/ui/pages/common/DeletePageModalContainer.js
@@ -12,7 +12,7 @@ export const mapStateToProps = state => ({
 
 export const mapDispatchToProps = dispatch => ({
   onConfirmDelete: (page, successRedirect) => {
-    dispatch(sendDeletePage(page, successRedirect));
+    dispatch(sendDeletePage({ ...page, redirectToPageTree: true }, successRedirect));
     dispatch(setVisibleModal(''));
   },
 });
