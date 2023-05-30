e8719f0f8a167d77c2e4ecd98ead6c23cdf052f2
DMinnai <d.minnai@entando.com>
2023-05-30 17:11:46 +0200

    ENG-4918-amend-caret-on-empty-page

--- src/ui/pages/common/PageTree.js
+++ src/ui/pages/common/PageTree.js
@@ -113,8 +113,7 @@ class PageTree extends Component {
           const className = ['PageTree__tree-column-td'];
 
           // Remove arrow from page with no child
-          // if (page.isEmpty || page.original.isEmpty) {
-          if (page.isEmpty) {
+          if (page.isEmpty || page.original.isEmpty) {
             className.push('PageTree__tree-column-td--empty');
           }
           // No drag class is added if first level child and Virtual Root On
