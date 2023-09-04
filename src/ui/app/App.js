88a949d86f43fc782a44ef7975c7c47fd9859e86
Ryan J. P. Burgos <ryanjpburgos@Ryans-MBP.fritz.box>
2023-09-04 16:59:04 +0200

    ENG-5139: add lazy loading to component-repository e adjust related unit tests

--- src/ui/app/App.js
+++ src/ui/app/App.js
@@ -135,12 +135,7 @@ import EditDataTypesPage from 'ui/data-types/edit/EditDataTypesPage';
 import AddDataTypeAttributePage from 'ui/data-types/attributes/AddDataTypeAttributePage';
 import EditDataTypeAttributePage from 'ui/data-types/attributes/EditDataTypeAttributePage';
 import MonolistPageContainer from 'ui/data-types/attributes/monolist/MonolistPageContainer';
-// component repository
-import ComponentListPage from 'ui/component-repository/components/list/ComponentListPage';
-import ComponentListPageDisabled from 'ui/component-repository/components/list/ComponentListPageDisabled';
-import SettingsListPage from 'ui/component-repository/settings/list/SettingsListPage';
-import SettingsEditPage from 'ui/component-repository/settings/edit/SettingsEditPage';
-import SettingsAddPage from 'ui/component-repository/settings/add/SettingsAddPage';
+
 import AddProfileTypeAttributePage from 'ui/profile-types/attributes/AddProfileTypeAttributePage';
 import EditProfileTypeAttributePage from 'ui/profile-types/attributes/EditProfileTypeAttributePage';
 import MonolistProfilePageContainer from 'ui/profile-types/attributes/monolist/MonolistProfilePageContainer';
@@ -190,6 +185,13 @@ const UploadFileBrowserPage = React.lazy(() => import('ui/file-browser/upload/Up
 const CreateFolderPage = React.lazy(() => import('ui/file-browser/add/CreateFolderPage'));
 const CreateTextFilePage = React.lazy(() => import('ui/file-browser/add/CreateTextFilePage'));
 const EditTextFilePage = React.lazy(() => import('ui/file-browser/edit/EditTextFilePage'));
+
+// component repository
+const ComponentListPage = React.lazy(() => import('ui/component-repository/components/list/ComponentListPage'));
+const ComponentListPageDisabled = React.lazy(() => import('ui/component-repository/components/list/ComponentListPageDisabled'));
+const SettingsListPage = React.lazy(() => import('ui/component-repository/settings/list/SettingsListPage'));
+const SettingsEditPage = React.lazy(() => import('ui/component-repository/settings/edit/SettingsEditPage'));
+const SettingsAddPage = React.lazy(() => import('ui/component-repository/settings/add/SettingsAddPage'));
 
 export const renderWithSuspense = component =>
   <Suspense fallback={<Spinner loading />}>{component}</Suspense>;
@@ -360,32 +362,24 @@ const RouteComponent = () => {
       <Route
         exact
         path={ROUTE_ECR_COMPONENT_LIST}
-        render={() => (
-          (COMPONENT_REPOSITORY_UI_ENABLED) ?
-            <ComponentListPage /> : <ComponentListPageDisabled />
-        )}
+        render={() => renderWithSuspense(COMPONENT_REPOSITORY_UI_ENABLED ?
+          <ComponentListPage /> : <ComponentListPageDisabled />)}
       />
       <Route
         exact
         path={ROUTE_ECR_CONFIG_LIST}
-        render={() => (
-          (COMPONENT_REPOSITORY_UI_ENABLED) ?
-            <SettingsListPage /> : <ComponentListPageDisabled />
-        )}
+        render={() => renderWithSuspense(COMPONENT_REPOSITORY_UI_ENABLED ?
+          <SettingsListPage /> : <ComponentListPageDisabled />)}
       />
       <Route
         path={ROUTE_ECR_CONFIG_EDIT}
-        render={() => (
-          (COMPONENT_REPOSITORY_UI_ENABLED) ?
-            <SettingsEditPage /> : <ComponentListPageDisabled />
-        )}
+        render={() => renderWithSuspense(COMPONENT_REPOSITORY_UI_ENABLED ?
+          <SettingsEditPage /> : <ComponentListPageDisabled />)}
       />
       <Route
         path={ROUTE_ECR_CONFIG_ADD}
-        render={() => (
-          (COMPONENT_REPOSITORY_UI_ENABLED) ?
-            <SettingsAddPage /> : <ComponentListPageDisabled />
-        )}
+        render={() => renderWithSuspense((COMPONENT_REPOSITORY_UI_ENABLED) ?
+          <SettingsAddPage /> : <ComponentListPageDisabled />)}
       />
       {/* email config */}
       <Route
