41206960b94f2198e7480bb4309d137aaf3fdfb7
ichalagashvili <i.chalagashvili@entando.com>
2023-09-14 12:07:59 +0200

    ENG-5150 remove redux form and fix the formi bugs in search form

--- src/ui/pages/list/PageSearchForm.js
+++ src/ui/pages/list/PageSearchForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Formik, Field } from 'formik';
+import { Field } from 'formik';
 import { FormattedMessage, defineMessages, injectIntl, intlShape } from 'react-intl';
 import { Row, Col, FormGroup, Button, DropdownButton, MenuItem } from 'patternfly-react';
 
@@ -24,79 +24,64 @@ const selectOptions = [
   },
 ];
 
-const selectedTypeSearchParamMap = {
-  code: 'pageCodeToken',
-  name: 'title',
-};
-
-export const PageSearchFormBody = ({ intl, onSubmit }) => {
-  const initialValues = {
-    pageCodeToken: '',
-    searchType: 'code',
-  };
-
-  const onformSubmit = (values) => {
-    const searchType = selectedTypeSearchParamMap[values.searchType];
-    onSubmit({ ...values, searchType });
-  };
-
-  return (
-    <Formik initialValues={initialValues} onSubmit={onformSubmit}>
-      {({ handleSubmit, handleChange, values }) => (
-        <form
-          onSubmit={handleSubmit}
-          className="PageSearchForm form-horizontal well"
-        >
-          <h3><FormattedMessage id="pageTree.searchForm.searchPageBy" /></h3>
-          <FormGroup>
-            <Row>
-              <Col xs={12} sm={3} className="PageSearchForm__filter-searchby">
-                <DropdownButton
-                  title={intl.formatMessage({ id: `pageTree.searchForm.${values.searchType}` })}
-                  id="attribute"
-                  onSelect={handleChange('searchType')}
-                  className="PageSearchForm__filter-searchby-dropdown"
-                >
-                  {selectOptions.map(option => (
-                    <MenuItem key={option.value} eventKey={option.value}>
-                      {intl.formatMessage({ id: option.label })}
-                    </MenuItem>
+export const PageSearchFormBody = ({
+  intl, handleSubmit, handleChange, values,
+}) => (
+  <form
+    onSubmit={handleSubmit}
+    className="PageSearchForm form-horizontal well"
+  >
+    <h3><FormattedMessage id="pageTree.searchForm.searchPageBy" /></h3>
+    <FormGroup>
+      <Row>
+        <Col xs={12} sm={3} className="PageSearchForm__filter-searchby">
+          <DropdownButton
+            title={intl.formatMessage({ id: `pageTree.searchForm.${values.searchType}` })}
+            id="attribute"
+            onSelect={handleChange('searchType')}
+            className="PageSearchForm__filter-searchby-dropdown"
+          >
+            {selectOptions.map(option => (
+              <MenuItem key={option.value} eventKey={option.value}>
+                {intl.formatMessage({ id: option.label })}
+              </MenuItem>
                   ))}
-                </DropdownButton>
-              </Col>
-              <Col xs={12} sm={8}>
-                <Field
-                  id="pagecode"
-                  type="input"
-                  className="form-control"
-                  name="pageCodeToken"
-                  placeholder={intl.formatMessage(msgs[values.searchType])}
-                />
-              </Col>
-            </Row>
-          </FormGroup>
-          <FormGroup>
-            <Row>
-              <Col xs={11}>
-                <Button
-                  type="submit"
-                  bsStyle="primary"
-                  className="pull-right PageSearchForm__save"
-                >
-                  <FormattedMessage id="app.search" />
-                </Button>
-              </Col>
-            </Row>
-          </FormGroup>
-        </form>
-      )}
-    </Formik>
-  );
-};
+          </DropdownButton>
+        </Col>
+        <Col xs={12} sm={8}>
+          <Field
+            id="pagecode"
+            type="input"
+            className="form-control"
+            name="pageCodeToken"
+            placeholder={intl.formatMessage(msgs[values.searchType])}
+          />
+        </Col>
+      </Row>
+    </FormGroup>
+    <FormGroup>
+      <Row>
+        <Col xs={11}>
+          <Button
+            type="submit"
+            bsStyle="primary"
+            className="pull-right PageSearchForm__save"
+          >
+            <FormattedMessage id="app.search" />
+          </Button>
+        </Col>
+      </Row>
+    </FormGroup>
+  </form>
+);
 
 PageSearchFormBody.propTypes = {
   intl: intlShape.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  values: PropTypes.shape({
+    searchType: PropTypes.string,
+  }).isRequired,
 };
 
 export default injectIntl(PageSearchFormBody);
