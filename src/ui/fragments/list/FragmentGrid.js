562d5ebc6fd5f35bd9e1a5d35279f6e9efdc32a9
Federico Gallucci <zyzyx85@gmail.com>
2025-01-30 17:22:42 +0100

    remove useless Component

--- src/ui/fragments/list/FragmentGrid.js
+++ src/ui/fragments/list/FragmentGrid.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { intlShape, injectIntl } from 'react-intl';
-import { Col, Spinner, PaginationRow } from 'patternfly-react';
+import {  Spinner, PaginationRow } from 'patternfly-react';
 
 import FragmentListMenuActions from 'ui/fragments/list/FragmentListMenuActions';
 import DeleteFragmentModalContainer from 'ui/fragments/list/DeleteFragmentModalContainer';
