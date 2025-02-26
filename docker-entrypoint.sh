ff03b435e55e9831ebe878a8d918509d7d448a6a
Walter Caffiero <w.caffiero@entando.com>
2025-02-26 17:39:17 +0100

    ENG-5543 Fixes another unbound variable issue when variable is not provided on environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 set -eu
 
-ENV_DOMAIN=${DOMAIN:-""}
-
 ENV_JSON='{'
-[ -n "$DOMAIN" ] && ENV_JSON+='"DOMAIN":"'"$ENV_DOMAIN"'", '
-[ -n "$DOMAIN_CM" ] && ENV_JSON+='"DOMAIN_CM":"'"$DOMAIN_CM"'", '
-[ -n "$KEYCLOAK_JSON" ] && ENV_JSON+='"KEYCLOAK_JSON":"'"$ENV_KEYCLOAK_JSON"'", '
-[ -n "$ENTANDO_VIRTUAL_CONTEXTS" ] && ENV_JSON+='"ENTANDO_VIRTUAL_CONTEXTS":"'"$ENTANDO_VIRTUAL_CONTEXTS"'"'
+[ -n "${DOMAIN:-""}" ] && ENV_JSON+='"DOMAIN":"'"$DOMAIN"'", '
+[ -n "${DOMAIN_CM:-""}" ] && ENV_JSON+='"DOMAIN_CM":"'"$DOMAIN_CM"'", '
+[ -n "${KEYCLOAK_JSON:-""}" ] && ENV_JSON+='"KEYCLOAK_JSON":"'"$KEYCLOAK_JSON"'", '
+[ -n "${ENTANDO_VIRTUAL_CONTEXTS:-""}" ] && ENV_JSON+='"ENTANDO_VIRTUAL_CONTEXTS":"'"$ENTANDO_VIRTUAL_CONTEXTS"'"'
 ENV_JSON+='}'
 
 ESCAPED_ENV_JSON=$(echo $ENV_JSON | sed 's/\"/\\\"/g' | sed 's/\//\\\//g' | tr -d '\n' | tr -d '[[:blank:]]')
