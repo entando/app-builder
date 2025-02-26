2d9ccac47139f7497f73ce8bb485e4bc1ff7db47
Walter Caffiero <w.caffiero@entando.com>
2025-02-26 11:18:44 +0100

    ENG-5543 updated docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,13 @@ then
 else
   ENV_KEYCLOAK_JSON="${KEYCLOAK_JSON}"
 fi
-ENV_JSON='{"DOMAIN":"'"$ENV_DOMAIN"'", "KEYCLOAK_JSON":"'"$ENV_KEYCLOAK_JSON"'"}'
+ENV_JSON='{'
+ENV_JSON+='"DOMAIN":"'"$ENV_DOMAIN"'", '
+ENV_JSON+='"DOMAIN_CM":"'"$DOMAIN_CM"'", '
+ENV_JSON+='"KEYCLOAK_JSON":"'"$ENV_KEYCLOAK_JSON"'", '
+ENV_JSON+='"ENTANDO_VIRTUAL_CONTEXTS":"'"$ENTANDO_VIRTUAL_CONTEXTS"'"'
+ENV_JSON+='}'
+
 ESCAPED_ENV_JSON=$(echo $ENV_JSON | sed 's/\"/\\\"/g' | sed 's/\//\\\//g' | tr -d '\n' | tr -d '[[:blank:]]')
 
 sed -i 's/"REACT_APP_ENV"/'"$ESCAPED_ENV_JSON"'/g' $HOME/app-builder/index.html
